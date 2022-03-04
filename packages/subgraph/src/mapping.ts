6380b5619ed18355c3491d33e3d86109e4805cd6
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-03-04 11:52:13 +0100

    feat(subgraph): add handler for offer creation and void (#2)
    * feat(subgraph): add handler for offer creation
    
    This adds the base entities of the subgraph
    and the event handler of the offer creation.
    Note, that this primarily targets local development.
    Therefore the `ipfs` data resolution is disabled.
    
    * feat(subgraph): add `OfferVoided` handler
    
    * feat(subgraph): populate exchange token data

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,5 +1,13 @@
-import { OfferCreated } from "../generated/IBosonOfferHandler/IBosonOfferHandler";
+import {
+  IBosonOfferHandler,
+  OfferCreated,
+  OfferVoided
+} from "../generated/OfferHandler/IBosonOfferHandler";
 import { Offer } from "../generated/schema";
+
+import { saveIpfsMetadata } from "./utils/metadata";
+import { saveExchangeToken } from "./utils/token";
+import { saveSeller } from "./utils/seller";
 
 export function handleOfferCreatedEvent(event: OfferCreated): void {
   const offerId = event.params.offerId;
@@ -7,8 +15,48 @@ export function handleOfferCreatedEvent(event: OfferCreated): void {
   let offer = Offer.load(offerId.toString());
 
   if (offer === null) {
-    offer = new Offer(offerId.toString());
-  }
+    const contract = IBosonOfferHandler.bind(event.address);
+    const result = contract.try_getOffer(offerId);
 
-  offer.save();
+    if (!result.reverted && result.value.value0) {
+      const offerFromContract = result.value.value1;
+
+      saveIpfsMetadata(offerFromContract.metadataHash, offerId.toString());
+      saveExchangeToken(offerFromContract.exchangeToken);
+      saveSeller(offerFromContract.seller);
+
+      offer = new Offer(offerId.toString());
+      offer.createdAt = event.block.timestamp;
+      offer.price = offerFromContract.price;
+      offer.deposit = offerFromContract.deposit;
+      offer.penalty = offerFromContract.penalty;
+      offer.quantity = offerFromContract.quantity;
+      offer.validFromDate = offerFromContract.validFromDate;
+      offer.validUntilDate = offerFromContract.validUntilDate;
+      offer.redeemableDate = offerFromContract.redeemableDate;
+      offer.fulfillmentPeriodDuration =
+        offerFromContract.fulfillmentPeriodDuration;
+      offer.voucherValidDuration = offerFromContract.voucherValidDuration;
+      offer.seller = offerFromContract.seller.toHexString();
+      offer.exchangeToken = offerFromContract.exchangeToken.toHexString();
+      offer.metadataUri = offerFromContract.metadataUri;
+      offer.metadataHash = offerFromContract.metadataHash;
+      offer.metadata = offerId.toString() + "-metadata";
+      offer.voided = false;
+
+      offer.save();
+    }
+  }
+}
+
+export function handleOfferVoidedEvent(event: OfferVoided): void {
+  const offerId = event.params.offerId;
+
+  const offer = Offer.load(offerId.toString());
+
+  if (offer !== null) {
+    offer.voided = true;
+    offer.voidedAt = event.block.timestamp;
+    offer.save();
+  }
 }
