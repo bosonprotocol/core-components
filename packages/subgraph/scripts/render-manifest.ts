4e78dc7761ab70b94d19e0465d1714591bcc52d1
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-03-07 19:17:41 +0100

    chore(subgraph): adapt template and scripts to changed common types

--- packages/subgraph/scripts/render-manifest.ts
+++ packages/subgraph/scripts/render-manifest.ts
@@ -1,12 +1,12 @@
 import fs from "fs";
 import handlebars from "handlebars";
 import { providers } from "ethers";
-import { getAddressesByEnv } from "../../common/src/addresses";
+import { getDefaultConfigByEnvName } from "../../common/src/configs";
 
 const generatedManifestsDir = __dirname + "/../generated/manifests";
 
 const envName = process.argv[2];
-const { protocolDiamond, chainId } = getAddressesByEnv(envName);
+const { contracts, chainId } = getDefaultConfigByEnvName(envName);
 
 const isLocalhost = chainId === 31337;
 
@@ -15,7 +15,7 @@ const manifestTemplate = fs.readFileSync(
 );
 const template = handlebars.compile(String(manifestTemplate));
 const manifest = template({
-  protocolDiamond,
+  protocolDiamond: contracts.protocolDiamond,
   network: isLocalhost ? "localhost" : providers.getNetwork(chainId).name,
   startBlock: isLocalhost ? 0 : 10143000
 });
