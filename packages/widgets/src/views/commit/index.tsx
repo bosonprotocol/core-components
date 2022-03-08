19890ef931868c923f5335905bfbec17976b759b
MariusAlch <alch.marius@gmail.com>
2022-03-08 15:20:27 +0200

    feat: static ui for create offer widget (#4)
    * library setup
    
    * base styling
    
    * UI improvements
    
    * Transaction processing modal
    
    * modal exmaple setup
    
    * update test command
    
    * change commit component name

---
+++ packages/widgets/src/views/commit/index.tsx
@@ -0,0 +1,9 @@
+import { WidgetLayout } from "../../lib/components/WidgetLayout";
+
+export function Commit() {
+  return (
+    <WidgetLayout title="Commit To Offer" offerName="Baggy Jeans">
+      CommitToOffer
+    </WidgetLayout>
+  );
+}
