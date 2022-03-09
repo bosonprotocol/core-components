9b8535f6064358ebfc3769575de8115e56386b05
Marius Alchimavicius <alch.marius@gmail.com>
2022-03-09 13:15:37 +0200

    Simple parent app UI setup

--- packages/example-parent-app/src/index.tsx
+++ packages/example-parent-app/src/index.tsx
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
--- packages/example-parent-app/src/views/create/index.tsx
+++ packages/example-parent-app/src/views/create/index.tsx
@@ -1,19 +1,67 @@
 import { createOffer } from "@bosonprotocol/widgets-sdk";
+import { Form, Button } from "react-bootstrap";
+import styled from "styled-components";
+import { useFormik } from "formik";
+
+const Root = styled.div`
+  padding: 24px;
+  max-width: 600px;
+`;
 
 export function HomeView() {
+  const formik = useFormik({
+    initialValues: {
+      title: "Baggy jeans",
+      price: "123",
+      quantity: "123"
+    },
+    onSubmit: (values) => {
+      createOffer({
+        ipfsCID: "1234",
+        price: "1234",
+        quantity: "1234"
+      });
+    }
+  });
+
   return (
-    <div>
-      <button
-        onClick={() =>
-          createOffer({
-            ipfsCID: "1234",
-            price: "1234",
-            quantity: "1234"
-          })
-        }
-      >
-        createoffer
-      </button>
-    </div>
+    <Root>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            name="title"
+            type="text"
+            placeholder="..."
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Price</Form.Label>
+          <Form.Control
+            value={formik.values.price}
+            onChange={formik.handleChange}
+            name="price"
+            type="text"
+            placeholder="..."
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Quantity</Form.Label>
+          <Form.Control
+            value={formik.values.quantity}
+            onChange={formik.handleChange}
+            name="quantity"
+            type="text"
+            placeholder="..."
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Root>
   );
 }
