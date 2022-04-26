10f081578598d9cc7a21eea9d8ccadbd8fb29875
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-04-26 16:48:02 +0200

    feat(subgraph): enrich metadata schemas with fields from offer (#68)
    The Graph currently does not support nested queries.
    As a workaround we enrich the `MetadataInterface` with respective fields
    from the related offer entity for improved filtering.

--- packages/subgraph/src/entities/metadata/base.ts
+++ packages/subgraph/src/entities/metadata/base.ts
@@ -1,22 +1,33 @@
 import { JSONValue, TypedMap } from "@graphprotocol/graph-ts";
+import { IBosonOfferHandler__getOfferResultOfferStruct } from "../../../generated/OfferHandler/IBosonOfferHandler";
 import { BaseMetadataEntity } from "../../../generated/schema";
 
 import { convertToString } from "../../utils/json";
 
 export function saveBaseMetadata(
-  offerId: string,
-  seller: string,
+  offerFromContract: IBosonOfferHandler__getOfferResultOfferStruct,
   metadataObj: TypedMap<string, JSONValue>
 ): string {
+  const offerId = offerFromContract.id.toString();
   const metadataId = offerId + "-metadata";
   const name = convertToString(metadataObj.get("name"));
   const description = convertToString(metadataObj.get("description"));
   const externalUrl = convertToString(metadataObj.get("external_url"));
   const schemaUrl = convertToString(metadataObj.get("schema_url"));
 
-  const baseMetadataEntity = new BaseMetadataEntity(metadataId);
+  let baseMetadataEntity = BaseMetadataEntity.load(metadataId);
+
+  if (baseMetadataEntity == null) {
+    baseMetadataEntity = new BaseMetadataEntity(metadataId);
+  }
+
   baseMetadataEntity.offer = offerId;
-  baseMetadataEntity.seller = seller;
+  baseMetadataEntity.seller = offerFromContract.seller.toHexString();
+  baseMetadataEntity.exchangeToken =
+    offerFromContract.exchangeToken.toHexString();
+  baseMetadataEntity.voided = offerFromContract.voided;
+  baseMetadataEntity.validFromDate = offerFromContract.validFromDate;
+  baseMetadataEntity.validUntilDate = offerFromContract.validUntilDate;
   baseMetadataEntity.type = "BASE";
   baseMetadataEntity.name = name;
   baseMetadataEntity.description = description;
--- packages/subgraph/src/entities/metadata/handler.ts
+++ packages/subgraph/src/entities/metadata/handler.ts
@@ -1,19 +1,19 @@
 import { log } from "@graphprotocol/graph-ts";
+import { IBosonOfferHandler__getOfferResultOfferStruct } from "../../../generated/OfferHandler/IBosonOfferHandler";
 import { saveProductV1Metadata } from "./product-v1";
 import { saveBaseMetadata } from "./base";
 import { getIpfsMetadataObject } from "../../utils/ipfs";
 import { convertToString } from "../../utils/json";
 
 export function saveMetadata(
-  ipfsHash: string,
-  offerId: string,
-  seller: string
+  offerFromContract: IBosonOfferHandler__getOfferResultOfferStruct
 ): string | null {
+  const ipfsHash = offerFromContract.metadataHash;
   const metadataObj = getIpfsMetadataObject(ipfsHash);
 
   if (metadataObj === null) {
     log.warning("Could not load metadata for offer with id: {}, ipfsHash: {}", [
-      offerId,
+      offerFromContract.id.toString(),
       ipfsHash
     ]);
     return null;
@@ -22,11 +22,11 @@ export function saveMetadata(
   const metadataType = convertToString(metadataObj.get("type"));
 
   if (metadataType == "BASE") {
-    return saveBaseMetadata(offerId, seller, metadataObj);
+    return saveBaseMetadata(offerFromContract, metadataObj);
   }
 
   if (metadataType == "PRODUCT_V1") {
-    return saveProductV1Metadata(offerId, seller, metadataObj);
+    return saveProductV1Metadata(offerFromContract, metadataObj);
   }
 
   return null;
