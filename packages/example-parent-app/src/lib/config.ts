5af0e2f5c4ab3c90a80b0ce323464c53690c7dc1
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-05-18 13:29:18 +0200

    feat: migrate to new testing env (#93)
    * feat: update testing env contract and json rpc
    
    * chore: ignore graph docker compose data
    
    * chore: fix local env
    
    * test: fix default config test

--- packages/example-parent-app/src/lib/config.ts
+++ packages/example-parent-app/src/lib/config.ts
@@ -1,7 +1,9 @@
 import { getDefaultConfig } from "@bosonprotocol/core-sdk";
 
+const envName = process.env.REACT_APP_ENV_NAME;
 const chainId = parseInt(process.env.REACT_APP_CHAIN_ID || "3");
 
 export const CONFIG = getDefaultConfig({
+  envName,
   chainId
 });
