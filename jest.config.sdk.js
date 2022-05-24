f386ed13e7bb37fe831913834cefd23a0d3aba2f
Rafał Mikucki <rafalmikucki@gmail.com>
2022-05-24 09:42:46 +0200

    feature: get each coverage

--- jest.config.sdk.js
+++ jest.config.sdk.js
@@ -4,6 +4,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.{ts,js,jsx,tsx}"],
+  coverageReporters: ["json", "json-summary"],
   coveragePathIgnorePatterns: ["jest.config.js", "/node_modules/", "/dist/"],
   moduleNameMapper: {
     "^@bosonprotocol/(.*)$": "<rootDir>/../$1/"
--- scripts/coverage.js
+++ scripts/coverage.js
@@ -2,19 +2,30 @@
 const fs = require("fs");
 const path = require("path");
 
-const PACKAGES_PATH = path.resolve(process.cwd(), "packages");
-const NYC_PATH = path.resolve(process.cwd(), ".nyc_output");
+const PATHS = {
+  PACKAGES: path.resolve(process.cwd(), "packages"),
+  NYC: path.resolve(process.cwd(), ".nyc_output"),
+  COVERAGE: path.resolve(process.cwd(), "coverage")
+};
+
+function copyFile({ packagePath, folder, name, dest, destName }) {
+  const file = path.resolve(packagePath, folder, name);
+  if (fs.existsSync(file)) {
+    const destPath = path.resolve(dest, `${destName}.json`);
+    fs.copyFileSync(file, destPath);
+  }
+}
 
 function combineCoverage() {
-  if (!fs.existsSync(NYC_PATH)) {
-    fs.mkdirSync(NYC_PATH);
+  if (!fs.existsSync(PATHS.NYC)) {
+    fs.mkdirSync(PATHS.NYC);
   }
 
-  fs.readdir(PACKAGES_PATH, (err, packages) => {
-    if (err) console.log(err);
+  fs.readdir(PATHS.PACKAGES, (err, packages) => {
+    if (err) console.error(err);
     else {
       packages.forEach((package) => {
-        const packagePath = path.resolve(PACKAGES_PATH, package);
+        const packagePath = path.resolve(PATHS.PACKAGES, package);
         fs.stat(packagePath, (err, stats) => {
           if (err) {
             console.log(err);
@@ -22,29 +33,28 @@ function combineCoverage() {
 
           if (stats.isDirectory()) {
             try {
-              try {
-                require("child_process").execSync(
-                  "npm run test --coverage --silent",
-                  {
-                    cwd: packagePath,
-                    stdio: "inherit"
-                  }
-                );
-              } catch (error) {
-                console.error("Failed to run test coverage", error);
-              }
-              const report = path.resolve(
-                packagePath,
-                "coverage",
-                "coverage-final.json"
-              );
-              if (fs.existsSync(report)) {
-                const destPath = path.resolve(NYC_PATH, `${package}.json`);
-                fs.copyFileSync(report, destPath);
-              }
+              require("child_process").execSync("npm run test --silent", {
+                cwd: packagePath,
+                stdio: "inherit"
+              });
             } catch (error) {
               console.log(error);
             }
+
+            copyFile({
+              packagePath,
+              folder: "coverage",
+              name: "coverage-final.json",
+              dest: PATHS.NYC,
+              destName: package
+            });
+            copyFile({
+              packagePath,
+              folder: "coverage",
+              name: "coverage-summary.json",
+              dest: PATHS.COVERAGE,
+              destName: `${package}-summary`
+            });
           }
         });
       });
