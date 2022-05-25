3d11a519d88be77534ef156ac70590b642f09dc0
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-05-25 10:37:15 +0200

    feat: json schema support for metadata (#95)
    We now use the JSON schema for different offer metadata types.
    But instead of completely switching over to ajv, I opted to use
    https://github.com/kristianmandrup/schema-to-yup.
    
    This allows us to use JSON schema with yup basically.
    So we don't have to ditch yup throughout our sdks, widgets and dapps.

--- packages/metadata/src/base/index.ts
+++ packages/metadata/src/base/index.ts
@@ -1,1 +1,13 @@
-export * from "./schema";
+import { buildYup } from "schema-to-yup";
+import { SchemaOf } from "yup";
+import schema from "./schema.json";
+
+export const baseMetadataSchema: SchemaOf<BaseMetadata> = buildYup(schema, {});
+
+export type BaseMetadata = {
+  name: string;
+  description: string;
+  externalUrl: string;
+  schemaUrl: string;
+  type: "BASE";
+};
--- packages/metadata/src/product-v1/index.ts
+++ packages/metadata/src/product-v1/index.ts
@@ -1,1 +1,19 @@
-export * from "./schema";
+import { buildYup } from "schema-to-yup";
+import { SchemaOf } from "yup";
+import schema from "./schema.json";
+
+export const productV1MetadataSchema: SchemaOf<ProductV1Metadata> = buildYup(
+  schema,
+  {}
+);
+
+export type ProductV1Metadata = {
+  name: string;
+  description: string;
+  externalUrl: string;
+  schemaUrl: string;
+  type: "PRODUCT_V1";
+  images: string[];
+  tags: string[];
+  brandName: string;
+};
