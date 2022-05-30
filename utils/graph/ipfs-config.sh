9a5c223815c44febf124162ef2a85ad6740e7f89
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-05-30 12:10:51 +0200

    fix: ipfs service in docker-compose
    The IPFS service was throwing `403: Forbidden` when trying to hit the API.
    We fix this by adding a script to the`container-init.d` folder which is
    part of v0.13 (not released yet).
    See https://github.com/ipfs/go-ipfs/issues/8640

---
+++ utils/graph/ipfs-config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Setting IPFS config..."
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
