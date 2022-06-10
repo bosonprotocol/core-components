e15595875c58f5e5acd366f232a2ba8e3fc32f36
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-06-10 22:39:25 +0200

    feat(common): update abis

--- packages/common/src/abis/index.ts
+++ packages/common/src/abis/index.ts
@@ -1,17 +1,19 @@
-import IBosonOfferHandlerABI from "./IBosonOfferHandler.json";
-import IBosonAccountHandlerABI from "./IBosonAccountHandler.json";
-import IBosonOrchestrationHandlerABI from "./IBosonOrchestrationHandler.json";
-import IBosonExchangeHandlerABI from "./IBosonExchangeHandler.json";
-
-import ProtocolDiamondABI from "./ProtocolDiamond.json";
+/** auto-generated code: do not edit */
 
 import ERC20ABI from "./ERC20.json";
+import IBosonAccountHandlerABI from "./IBosonAccountHandler.json";
+import IBosonExchangeHandlerABI from "./IBosonExchangeHandler.json";
+import IBosonFundsHandlerABI from "./IBosonFundsHandler.json";
+import IBosonOfferHandlerABI from "./IBosonOfferHandler.json";
+import IBosonOrchestrationHandlerABI from "./IBosonOrchestrationHandler.json";
+import ProtocolDiamondABI from "./ProtocolDiamond.json";
 
 export {
-  IBosonOfferHandlerABI,
+  ERC20ABI,
   IBosonAccountHandlerABI,
-  IBosonOrchestrationHandlerABI,
   IBosonExchangeHandlerABI,
-  ProtocolDiamondABI,
-  ERC20ABI
+  IBosonFundsHandlerABI,
+  IBosonOfferHandlerABI,
+  IBosonOrchestrationHandlerABI,
+  ProtocolDiamondABI
 };
