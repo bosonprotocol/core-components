9facef2c788f50852ee7780a02f5a73b60fbcc9c
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-06-20 11:55:25 +0200

    test(e2e): add tests for revoke, cancel, redeem

--- e2e/tests/core-sdk.test.ts
+++ e2e/tests/core-sdk.test.ts
@@ -1,175 +1,234 @@
-import { providers, Wallet, utils } from "ethers";
-import { CoreSDK, getDefaultConfig } from "../../packages/core-sdk/src";
-import { IpfsMetadataStorage } from "../../packages/ipfs-storage/src";
+import { utils, constants, BigNumberish } from "ethers";
+
 import { mockCreateOfferArgs } from "../../packages/common/tests/mocks";
-import { EthersAdapter } from "../../packages/ethers-sdk/src";
-import { ACCOUNT_1, ACCOUNT_2 } from "../../contracts/accounts";
+import { CoreSDK } from "../../packages/core-sdk/src";
+import { ExchangeState } from "../../packages/core-sdk/src/subgraph";
+
+import {
+  initCoreSDKWithFundedWallet,
+  initSellerAndBuyerSDKs,
+  metadata,
+  waitForGraphNodeIndexing
+} from "./utils";
 
 jest.setTimeout(60_000);
 
-const defaultConfig = getDefaultConfig({
-  envName: "local"
-});
-
-const provider = new providers.JsonRpcProvider(defaultConfig.jsonRpcUrl);
-const seedWallet1 = new Wallet(ACCOUNT_1.privateKey, provider);
-const seedWallet2 = new Wallet(ACCOUNT_2.privateKey, provider);
-
-const ipfsMetadataStorage = new IpfsMetadataStorage({
-  url: defaultConfig.ipfsMetadataUrl
-});
-const graphMetadataStorage = new IpfsMetadataStorage({
-  url: defaultConfig.theGraphIpfsUrl
-});
-
 describe("core-sdk", () => {
   describe("core user flows", () => {
-    test("create seller + offer -> deposit seller funds -> commit", async () => {
-      const metadata = {
-        name: "name",
-        description: "description",
-        externalUrl: "external-url.com",
-        schemaUrl: "schema-url.com"
-      };
+    test("create seller + offer", async () => {
+      const { coreSDK, fundedWallet } = await initCoreSDKWithFundedWallet();
+
+      const createdOffer = await createSellerAndOffer(
+        coreSDK,
+        fundedWallet.address
+      );
+
+      expect(createdOffer).toBeTruthy();
+      expect(createdOffer.metadata).toMatchObject(metadata);
+      expect(createdOffer.seller).toBeTruthy();
+      expect(createdOffer.seller.operator.toLowerCase()).toBe(
+        fundedWallet.address.toLowerCase()
+      );
+    });
+
+    test("deposit seller funds", async () => {
       const sellerFundsDepositInEth = "5";
-      const [newSeller, buyer] = await Promise.all([
-        createFundedWallet(seedWallet1),
-        createFundedWallet(seedWallet2)
-      ]);
-      const sellerCoreSDK = CoreSDK.fromDefaultConfig({
-        envName: "local",
-        web3Lib: new EthersAdapter(provider, newSeller),
-        metadataStorage: ipfsMetadataStorage,
-        theGraphStorage: graphMetadataStorage
-      });
-      const buyerCoreSDK = CoreSDK.fromDefaultConfig({
-        envName: "local",
-        web3Lib: new EthersAdapter(provider, buyer),
-        metadataStorage: ipfsMetadataStorage,
-        theGraphStorage: graphMetadataStorage
-      });
-
-      /**
-       * Step 1: Store metadata on ipfs
-       */
-      const metadataHash = await ipfsMetadataStorage.storeMetadata({
-        ...metadata,
-        type: "BASE"
-      });
-      const metadataUri = "ipfs://" + metadataHash;
-
-      expect(typeof metadataHash).toBe("string");
-
-      /**
-       * Step 2-1: Create new seller account and offer
-       */
-      const createOfferTxResponse = await sellerCoreSDK.createSellerAndOffer(
-        {
-          operator: newSeller.address,
-          admin: newSeller.address,
-          clerk: newSeller.address,
-          treasury: newSeller.address
-        },
-        mockCreateOfferArgs({
-          offerChecksum: metadataHash,
-          metadataUri
-        })
-      );
-      const createOfferTxReceipt = await createOfferTxResponse.wait();
-      const createdOfferId = sellerCoreSDK.getCreatedOfferIdFromLogs(
-        createOfferTxReceipt.logs
+      const { coreSDK, fundedWallet } = await initCoreSDKWithFundedWallet();
+      const createdOffer = await createSellerAndOffer(
+        coreSDK,
+        fundedWallet.address
       );
 
-      expect(createdOfferId).toBeTruthy();
-
-      /**
-       * Step 2-2: Assert that subgraph indexed `Offer` and `Seller` entities
-       */
-      await waitForGraphNodeIndexing();
-      const [offer, seller] = await Promise.all([
-        sellerCoreSDK.getOfferById(createdOfferId || 0),
-        sellerCoreSDK.getSellerByAddress(newSeller.address)
-      ]);
-
-      expect(offer).toBeTruthy();
-      expect(offer.metadata).toMatchObject(metadata);
-      expect(offer.metadataUri).toBe(metadataUri);
-      expect(seller).toBeTruthy();
-      expect(seller.operator.toLowerCase()).toBe(
-        newSeller.address.toLowerCase()
-      );
-
-      /**
-       * Step 3-1: Deposit seller funds
-       */
-      const depositFundsTxResponse = await sellerCoreSDK.depositFunds(
-        offer.seller.id,
-        utils.parseEther(sellerFundsDepositInEth)
-      );
-      const depositFundsTxReceipt = await depositFundsTxResponse.wait();
-
-      expect(depositFundsTxReceipt.status).toBeTruthy();
-
-      /**
-       * Step 3-2: Assert that subgraph indexed `FundsEntity`
-       */
-      await waitForGraphNodeIndexing();
-
-      const funds = await sellerCoreSDK.getFunds({
-        fundsFilter: {
-          accountId: offer.seller.id
-        }
+      const funds = await depositFunds({
+        coreSDK,
+        fundsDepositAmountInEth: sellerFundsDepositInEth,
+        sellerId: createdOffer.seller.id
       });
 
-      expect(funds.length).toBe(1);
-      expect(funds[0].availableAmount).toBe(
+      expect(funds).toBeTruthy();
+      expect(funds.availableAmount).toBe(
         utils.parseEther(sellerFundsDepositInEth).toString()
       );
-      expect(funds[0].token.symbol.toUpperCase()).toBe("ETH");
+      expect(funds.token.symbol.toUpperCase()).toBe("ETH");
+    });
 
-      /**
-       * Step 4-1: Commit to offer
-       */
-      const commitToOfferTxResponse = await buyerCoreSDK.commitToOffer(
-        offer.id
+    test("commit", async () => {
+      const { sellerCoreSDK, sellerWallet, buyerCoreSDK } =
+        await initSellerAndBuyerSDKs();
+      const createdOffer = await createSellerAndOffer(
+        sellerCoreSDK,
+        sellerWallet.address
       );
-      const commitToOfferTxReceipt = await commitToOfferTxResponse.wait();
-      const exchangeId = buyerCoreSDK.getCommittedExchangeIdFromLogs(
-        commitToOfferTxReceipt.logs
+      await depositFunds({
+        coreSDK: sellerCoreSDK,
+        sellerId: createdOffer.seller.id
+      });
+
+      const exchange = await commitToOffer({
+        buyerCoreSDK,
+        sellerCoreSDK,
+        offerId: createdOffer.id
+      });
+
+      expect(exchange).toBeTruthy();
+    });
+
+    test("revoke", async () => {
+      const { sellerCoreSDK, sellerWallet, buyerCoreSDK } =
+        await initSellerAndBuyerSDKs();
+      const createdOffer = await createSellerAndOffer(
+        sellerCoreSDK,
+        sellerWallet.address
+      );
+      await depositFunds({
+        coreSDK: sellerCoreSDK,
+        sellerId: createdOffer.seller.id
+      });
+      const exchange = await commitToOffer({
+        buyerCoreSDK,
+        sellerCoreSDK,
+        offerId: createdOffer.id
+      });
+
+      const txResponse = await sellerCoreSDK.revokeVoucher(exchange.id);
+      await txResponse.wait();
+      await waitForGraphNodeIndexing();
+      const exchangeAfterRevoke = await sellerCoreSDK.getExchangeById(
+        exchange.id
       );
 
-      expect(exchangeId).toBeTruthy();
+      expect(exchangeAfterRevoke.state).toBe(ExchangeState.Revoked);
+      expect(exchangeAfterRevoke.revokedDate).toBeTruthy();
+    });
 
-      /**
-       * Step 4-2: Assert that subgraph indexed `Exchange` entity
-       */
-      // TODO
+    test("cancel", async () => {
+      const { sellerCoreSDK, sellerWallet, buyerCoreSDK } =
+        await initSellerAndBuyerSDKs();
+      const createdOffer = await createSellerAndOffer(
+        sellerCoreSDK,
+        sellerWallet.address
+      );
+      await depositFunds({
+        coreSDK: sellerCoreSDK,
+        sellerId: createdOffer.seller.id
+      });
+      const exchange = await commitToOffer({
+        buyerCoreSDK,
+        sellerCoreSDK,
+        offerId: createdOffer.id
+      });
+
+      const txResponse = await buyerCoreSDK.cancelVoucher(exchange.id);
+      await txResponse.wait();
+      await waitForGraphNodeIndexing();
+      const exchangeAfterRevoke = await buyerCoreSDK.getExchangeById(
+        exchange.id
+      );
+
+      expect(exchangeAfterRevoke.state).toBe(ExchangeState.Cancelled);
+      expect(exchangeAfterRevoke.cancelledDate).toBeTruthy();
+    });
+
+    test("redeem", async () => {
+      const { sellerCoreSDK, sellerWallet, buyerCoreSDK } =
+        await initSellerAndBuyerSDKs();
+      const createdOffer = await createSellerAndOffer(
+        sellerCoreSDK,
+        sellerWallet.address
+      );
+      await depositFunds({
+        coreSDK: sellerCoreSDK,
+        sellerId: createdOffer.seller.id
+      });
+      const exchange = await commitToOffer({
+        buyerCoreSDK,
+        sellerCoreSDK,
+        offerId: createdOffer.id
+      });
+
+      const txResponse = await buyerCoreSDK.redeemVoucher(exchange.id);
+      await txResponse.wait();
+      await waitForGraphNodeIndexing();
+      const exchangeAfterRevoke = await buyerCoreSDK.getExchangeById(
+        exchange.id
+      );
+
+      expect(exchangeAfterRevoke.state).toBe(ExchangeState.Redeemed);
+      expect(exchangeAfterRevoke.redeemedDate).toBeTruthy();
     });
   });
 });
 
-async function waitForGraphNodeIndexing() {
-  await wait(5_000);
+async function createSellerAndOffer(coreSDK: CoreSDK, sellerAddress: string) {
+  const metadataHash = await coreSDK.storeMetadata({
+    ...metadata,
+    type: "BASE"
+  });
+  const metadataUri = "ipfs://" + metadataHash;
+
+  const createOfferTxResponse = await coreSDK.createSellerAndOffer(
+    {
+      operator: sellerAddress,
+      admin: sellerAddress,
+      clerk: sellerAddress,
+      treasury: sellerAddress
+    },
+    mockCreateOfferArgs({
+      offerChecksum: metadataHash,
+      metadataUri
+    })
+  );
+  const createOfferTxReceipt = await createOfferTxResponse.wait();
+  const createdOfferId = coreSDK.getCreatedOfferIdFromLogs(
+    createOfferTxReceipt.logs
+  );
+
+  await waitForGraphNodeIndexing();
+  const offer = await coreSDK.getOfferById(createdOfferId as string);
+
+  return offer;
 }
 
-async function wait(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
+async function depositFunds(args: {
+  coreSDK: CoreSDK;
+  sellerId: string;
+  fundsDepositAmountInEth?: string;
+}) {
+  const depositFundsTxResponse = await args.coreSDK.depositFunds(
+    args.sellerId,
+    utils.parseEther(args.fundsDepositAmountInEth || "5")
+  );
+  await depositFundsTxResponse.wait();
+
+  await waitForGraphNodeIndexing();
+  const funds = await args.coreSDK.getFunds({
+    fundsFilter: {
+      accountId: args.sellerId,
+      token: constants.AddressZero
+    }
   });
+
+  return funds[0];
 }
 
-async function createFundedWallet(
-  fundingWallet: Wallet,
-  fundAmountInEth = "10"
-) {
-  const fundedWallet = Wallet.createRandom({
-    provider: fundingWallet.provider
-  });
-  const fundingTx = await fundingWallet.sendTransaction({
-    value: utils.parseEther(fundAmountInEth),
-    to: fundedWallet.address
-  });
-  fundingTx.wait();
+async function commitToOffer(args: {
+  buyerCoreSDK: CoreSDK;
+  sellerCoreSDK: CoreSDK;
+  offerId: BigNumberish;
+}) {
+  const commitToOfferTxResponse = await args.buyerCoreSDK.commitToOffer(
+    args.offerId
+  );
+  const commitToOfferTxReceipt = await commitToOfferTxResponse.wait();
+  const exchangeId = args.buyerCoreSDK.getCommittedExchangeIdFromLogs(
+    commitToOfferTxReceipt.logs
+  );
 
-  return fundedWallet;
+  expect(exchangeId).toBeTruthy();
+
+  await waitForGraphNodeIndexing();
+  const exchange = await args.sellerCoreSDK.getExchangeById(
+    exchangeId as string
+  );
+  return exchange;
 }
---
+++ e2e/tests/utils.ts
@@ -0,0 +1,89 @@
+import { providers, Wallet, utils } from "ethers";
+import { CoreSDK, getDefaultConfig } from "../../packages/core-sdk/src";
+import { IpfsMetadataStorage } from "../../packages/ipfs-storage/src";
+import { EthersAdapter } from "../../packages/ethers-sdk/src";
+import { ACCOUNT_1, ACCOUNT_2 } from "../../contracts/accounts";
+
+export const metadata = {
+  name: "name",
+  description: "description",
+  externalUrl: "external-url.com",
+  schemaUrl: "schema-url.com"
+};
+export const sellerFundsDepositInEth = "5";
+
+export const defaultConfig = getDefaultConfig({
+  envName: "local"
+});
+
+export const provider = new providers.JsonRpcProvider(defaultConfig.jsonRpcUrl);
+export const seedWallet1 = new Wallet(ACCOUNT_1.privateKey, provider);
+export const seedWallet2 = new Wallet(ACCOUNT_2.privateKey, provider);
+
+export const ipfsMetadataStorage = new IpfsMetadataStorage({
+  url: defaultConfig.ipfsMetadataUrl
+});
+export const graphMetadataStorage = new IpfsMetadataStorage({
+  url: defaultConfig.theGraphIpfsUrl
+});
+
+export async function initSellerAndBuyerSDKs(
+  args: Partial<{
+    seedWalletForSeller: Wallet;
+    seedWalletForBuyer: Wallet;
+  }> = {}
+) {
+  const [
+    { coreSDK: sellerCoreSDK, fundedWallet: sellerWallet },
+    { coreSDK: buyerCoreSDK, fundedWallet: buyerWallet }
+  ] = await Promise.all([
+    initCoreSDKWithFundedWallet(args.seedWalletForSeller || seedWallet1),
+    initCoreSDKWithFundedWallet(args.seedWalletForSeller || seedWallet2)
+  ]);
+
+  return {
+    sellerCoreSDK,
+    buyerCoreSDK,
+    sellerWallet,
+    buyerWallet
+  };
+}
+
+export async function initCoreSDKWithFundedWallet(
+  seedWallet: Wallet = seedWallet1
+) {
+  const fundedWallet = await createFundedWallet(seedWallet || seedWallet1);
+  const coreSDK = CoreSDK.fromDefaultConfig({
+    envName: "local",
+    web3Lib: new EthersAdapter(provider, fundedWallet),
+    metadataStorage: ipfsMetadataStorage,
+    theGraphStorage: graphMetadataStorage
+  });
+  return { coreSDK, fundedWallet };
+}
+
+export async function waitForGraphNodeIndexing() {
+  await wait(3_000);
+}
+
+export async function wait(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function createFundedWallet(
+  fundingWallet: Wallet,
+  fundAmountInEth = "10"
+) {
+  const fundedWallet = Wallet.createRandom({
+    provider: fundingWallet.provider
+  });
+  const fundingTx = await fundingWallet.sendTransaction({
+    value: utils.parseEther(fundAmountInEth),
+    to: fundedWallet.address
+  });
+  fundingTx.wait();
+
+  return fundedWallet;
+}
