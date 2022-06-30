3f07777406cba0ec9ef48b9f5bc9a72364d075c3
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2022-06-30 17:08:04 +0100

    add tests for missing metadata arguments

--- packages/metadata/tests/index.test.ts
+++ packages/metadata/tests/index.test.ts
@@ -2,6 +2,183 @@ import { validateMetadata, AnyMetadata } from "../src/index";
 import productV1ValidFullOffer from "./product-v1/valid/fullOffer.json";
 import productV1ValidMinimalOffer from "./product-v1/valid/minimalOffer.json";
 
+const productMissingArguments = [
+  {
+    arg: "schemaUrl",
+    data: { schemaUrl: undefined },
+    error: /schemaUrl is a required field/
+  },
+  {
+    arg: "type",
+    data: { type: undefined },
+    error: /Metadata validation failed for unknown type/
+  },
+  {
+    arg: "uuid",
+    data: { uuid: undefined },
+    error: /uuid is a required field/
+  },
+  {
+    arg: "name",
+    data: { name: undefined },
+    error: /name is a required field/
+  },
+  {
+    arg: "description",
+    data: { description: undefined },
+    error: /description is a required field/
+  },
+  {
+    arg: "externalUrl",
+    data: { externalUrl: undefined },
+    error: /externalUrl is a required field/
+  },
+  {
+    arg: "image",
+    data: { image: undefined },
+    error: /image is a required field/
+  },
+  {
+    arg: "attributes",
+    data: { attributes: undefined },
+    error: /attributes is a required field/
+  },
+  { arg: "product", data: { product: undefined } },
+  {
+    arg: "exchangePolicy",
+    data: { exchangePolicy: undefined }
+  },
+  {
+    arg: "exchangePolicy.uuid",
+    data: { exchangePolicy: { uuid: undefined } },
+    error: /exchangePolicy.uuid is a required field/
+  },
+  {
+    arg: "exchangePolicy.version",
+    data: { exchangePolicy: { version: undefined } },
+    error: /exchangePolicy.version is a required field/
+  },
+  {
+    arg: "exchangePolicy.version not a number",
+    data: { exchangePolicy: { version: "not_a_number" } },
+    error: /exchangePolicy.version must be a \`number\` type/
+  },
+  {
+    arg: "exchangePolicy.template",
+    data: { exchangePolicy: { template: undefined } },
+    error: /exchangePolicy.template is a required field/
+  },
+  {
+    arg: "product.uuid",
+    data: { product: { uuid: undefined } },
+    error: /product.uuid is a required field/
+  },
+  {
+    arg: "product.version",
+    data: { product: { version: undefined } },
+    error: /product.version is a required field/
+  },
+  {
+    arg: "product.version not a number",
+    data: { product: { version: "not_a_number" } },
+    error: /product.version must be a \`number\` type/
+  },
+  {
+    arg: "product.title",
+    data: { product: { title: undefined } },
+    error: /product.title is a required field/
+  },
+  {
+    arg: "product.description",
+    data: { product: { description: undefined } },
+    error: /product.description is a required field/
+  },
+  {
+    arg: "product.productionInformation",
+    data: { product: { productionInformation: undefined } }
+  },
+  {
+    arg: "product.productionInformation.brandName",
+    data: { product: { productionInformation: { brandName: undefined } } },
+    error: /product.productionInformation.brandName is a required field/
+  },
+  {
+    arg: "product.details",
+    data: { product: { details: undefined } }
+  },
+  {
+    arg: "product.details.offerCategory",
+    data: { product: { details: { offerCategory: undefined } } },
+    error: /product.details.offerCategory is a required field/
+  },
+  {
+    arg: "product.visuals",
+    data: { product: { visuals: undefined } }
+  },
+  {
+    arg: "variations[x].type",
+    data: { variations: [{ option: "anOption" }] },
+    error: /variations\[0\].type is a required field/
+  },
+  {
+    arg: "variations[x].option",
+    data: { variations: [{ type: "aType" }] },
+    error: /variations\[0\].option is a required field/
+  },
+  {
+    arg: "seller.images[x].url",
+    data: { seller: { images: [{ tag: "aTag" }] } },
+    error: /seller.images\[0\].url is a required field/
+  },
+  {
+    arg: "seller.contactLinks[x].url",
+    data: { seller: { contactLinks: [{ tag: "aTag" }] } },
+    error: /seller.contactLinks\[0\].url is a required field/
+  },
+  {
+    arg: "seller.contactLinks[x].tag",
+    data: { seller: { contactLinks: [{ url: "example.com" }] } },
+    error: /seller.contactLinks\[0\].tag is a required field/
+  },
+  {
+    arg: "shipping.supportedJurisdictions[x].label",
+    data: {
+      shipping: { supportedJurisdictions: [{ deliveryTime: "very soon" }] }
+    },
+    error: /shipping.supportedJurisdictions\[0\].label is a required field/
+  },
+  {
+    arg: "shipping.supportedJurisdictions[x].deliveryTime",
+    data: {
+      shipping: { supportedJurisdictions: [{ label: "aLabel" }] }
+    },
+    error:
+      /shipping.supportedJurisdictions\[0\].deliveryTime is a required field/
+  }
+  // {
+  //   arg: "attributes",
+  //   data: {
+  //     attributes: [
+  //       {
+  //         trait_type: "Offer Category",
+  //         value: "PHYSICAL"
+  //       }
+  //     ]
+  //   }
+  // } // it should fail because minItems is set to 2 (does not work as expected...)
+
+  // {
+  //   arg: "product.visuals.images",
+  //   data: { product: { visuals: { images: [] } } }
+  // } // it should fail because minItems is set to 1 (does not work as expected...)
+
+  // {
+  //   arg: "seller",
+  //   data: { seller: undefined },
+  //   error: /seller is a required field/
+  // } // it should fail because seller is a required field (not as expected)
+];
+
 describe("#validateMetadata()", () => {
   test("throw for invalid type", () => {
     expect(() =>
@@ -52,6 +229,113 @@ describe("#validateMetadata()", () => {
       expect(
         validateMetadata(productV1ValidMinimalOffer as any as AnyMetadata)
       ).toBeTruthy();
+    });
+
+    test.each(productMissingArguments)(
+      "throw for missing argument '$arg'",
+      ({ arg, data, error }) => {
+        const product = (data as any).product
+          ? ((data as any).product as any).productionInformation
+            ? {
+                ...productV1ValidFullOffer,
+                ...data,
+                product: {
+                  ...productV1ValidFullOffer.product,
+                  productionInformation: {
+                    ...productV1ValidFullOffer.product.productionInformation,
+                    ...((data as any).product as any).productionInformation
+                  }
+                }
+              }
+            : ((data as any).product as any).details
+            ? {
+                ...productV1ValidFullOffer,
+                ...data,
+                product: {
+                  ...productV1ValidFullOffer.product,
+                  details: {
+                    ...productV1ValidFullOffer.product.details,
+                    ...((data as any).product as any).details
+                  }
+                }
+              }
+            : (((data as any).product as any)?.packaging as any)?.dimensions
+            ? {
+                ...productV1ValidFullOffer,
+                ...data,
+                product: {
+                  ...productV1ValidFullOffer.product,
+                  packaging: {
+                    ...productV1ValidFullOffer.product.packaging,
+                    dimensions: {
+                      ...productV1ValidFullOffer.product.packaging.dimensions,
+                      ...(((data as any).product as any).packaging as any)
+                        .dimensions
+                    }
+                  }
+                }
+              }
+            : {
+                ...productV1ValidFullOffer,
+                ...data,
+                product: {
+                  ...productV1ValidFullOffer.product,
+                  ...(data as any).product
+                }
+              }
+          : (data as any).exchangePolicy
+          ? {
+              ...productV1ValidFullOffer,
+              ...data,
+              exchangePolicy: {
+                ...productV1ValidFullOffer.exchangePolicy,
+                ...(data as any).exchangePolicy
+              }
+            }
+          : {
+              ...productV1ValidFullOffer,
+              ...data
+            };
+        expect(() => validateMetadata(product as any as AnyMetadata)).toThrow(
+          error
+        );
+      }
+    );
+
+    xtest("throw if missing 'Redeemable Until' attribute", () => {
+      const attributes = productV1ValidFullOffer.attributes.map((attr) => {
+        if (attr.trait_type === "Redeemable Until") {
+          return {
+            ...attr,
+            trait_type: "XXXXXX"
+          };
+        }
+        return attr;
+      });
+      expect(() =>
+        validateMetadata({
+          ...productV1ValidFullOffer,
+          attributes
+        } as any as AnyMetadata)
+      ).toThrow();
+    });
+
+    xtest("throw if missing 'Offer Category' attribute", () => {
+      const attributes = productV1ValidFullOffer.attributes.map((attr) => {
+        if (attr.trait_type === "Offer Category") {
+          return {
+            ...attr,
+            trait_type: "XXXXXX"
+          };
+        }
+        return attr;
+      });
+      expect(() =>
+        validateMetadata({
+          ...productV1ValidFullOffer,
+          attributes
+        } as any as AnyMetadata)
+      ).toThrow();
     });
   });
 });
