4826137ab010acde294916fb30425dfeddbd926a
Roberto Sousa <beto.sousa22@gmail.com>
2022-07-05 15:30:39 +0100

    style: new component
    button component

---
+++ packages/visual-components/src/components/button/CommitButton.tsx
@@ -0,0 +1,113 @@
+import { CoreSDK } from "@bosonprotocol/core-sdk";
+import { EthersAdapter } from "@bosonprotocol/ethers-sdk";
+import { providers } from "ethers";
+import { useState } from "react";
+
+import Button from "./button";
+import { useMetaTxHandlerContract } from "../../lib/meta-transactions/useMetaTxHandlerContract";
+import { hooks } from "../../lib/connectors/metamask";
+import { useCoreSDK } from "../../lib/useCoreSDK";
+
+interface CommitButtonProps {
+  offerId: string;
+  web3Provider: providers.Web3Provider;
+  chainId: number;
+  subgraphUrl?: string;
+  protocolDiamond?: string;
+  metaTransactionApiKey?: string;
+  theGraphStorage?: string;
+  onPending: ({
+    offerId,
+    isLoading
+  }: {
+    offerId: string;
+    isLoading: boolean;
+  }) => void;
+  onSuccess: ({
+    offerId,
+    txHash,
+    exchangeId
+  }: {
+    offerId: string;
+    txHash: string;
+    exchangeId: string;
+  }) => void;
+  onError: ({ offerId, message }: { offerId: string; message: string }) => void;
+}
+
+const CommitButton = ({
+  offerId,
+  web3Provider,
+  chainId,
+  subgraphUrl,
+  protocolDiamond,
+  metaTransactionApiKey,
+  onPending,
+  onSuccess,
+  onError
+}: CommitButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const metaTxContract = useMetaTxHandlerContract();
+  const account = hooks.useAccount();
+
+  return (
+    <Button
+      variant="primary"
+      onClick={async () => {
+        console.log("on button click");
+
+        const coreSDK = CoreSDK.fromDefaultConfig({
+          web3Lib: new EthersAdapter(web3Provider),
+          chainId: chainId
+        });
+
+        try {
+          setIsLoading(true);
+          onPending({ offerId, isLoading });
+
+          let txResponse;
+
+          if (metaTransactionApiKey && metaTxContract && account) {
+            const nonce = Date.now();
+
+            const { r, s, v } = await coreSDK.signExecuteMetaTxCommitToOffer({
+              chainId,
+              offerId,
+              nonce
+            });
+
+            txResponse = await metaTxContract.executeMetaTxCommitToOffer(
+              account,
+              { buyer: account, offerId },
+              nonce,
+              r,
+              s,
+              v
+            );
+          } else {
+            txResponse = await coreSDK.commitToOffer(offerId);
+          }
+
+          const txReceipt = await txResponse.wait(1);
+
+          const txHash = txResponse.hash;
+          const exchangeId = coreSDK.getCommittedExchangeIdFromLogs(
+            txReceipt.logs
+          );
+
+          onSuccess({ offerId, txHash, exchangeId });
+          setIsLoading(false);
+          onPending({ offerId, isLoading });
+        } catch (error) {
+          setIsLoading(false);
+          onPending({ offerId, isLoading });
+          onError({ offerId, message: "error commiting the item" });
+        }
+      }}
+    >
+      Commit
+    </Button>
+  );
+};
+
+export default CommitButton;
--- packages/visual-components/src/views/main-page/index.tsx
+++ packages/visual-components/src/views/main-page/index.tsx
@@ -1,9 +1,54 @@
 import styled from "styled-components";
+import { ethers } from "ethers";
+
 import Card from "../../components/card/card";
+import CommitButton from "../../components/button/CommitButton";
 
 export default function MainPage() {
+  // ts-ignore
+  const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+
   return (
     <MainPageContainer>
+      <CommitButton
+        offerId="0x0"
+        web3Provider={web3Provider}
+        chainId={0}
+        onSuccess={({ offerId, txHash }) => {
+          console.log("on success");
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 32 ~ MainPage ~ txHash",
+            txHash
+          );
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 32 ~ MainPage ~ offerId",
+            offerId
+          );
+        }}
+        onError={({ offerId, message }) => {
+          console.log("on error");
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 29 ~ MainPage ~ message",
+            message
+          );
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 29 ~ MainPage ~ offerId",
+            offerId
+          );
+        }}
+        onPending={({ offerId, isLoading }) => {
+          console.log("on pending");
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 32 ~ MainPage ~ isLoading",
+            isLoading
+          );
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 32 ~ MainPage ~ offerId",
+            offerId
+          );
+        }}
+      />
+
       <Card
         image="https://images.unsplash.com/photo-1636718282214-0b4162a154f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=302&q=80"
         link="#"
@@ -23,5 +68,5 @@ const MainPageContainer = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background-color: ${({ theme }) => theme?.colors?.light.primary};
+  background-color: ${({ theme }) => theme?.colors?.light.white};
 `;
