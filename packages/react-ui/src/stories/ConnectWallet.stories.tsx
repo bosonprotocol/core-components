31e57576f394096ee9af7b7836170d2887f6304f
Roberto Sousa <beto.sousa22@gmail.com>
2022-07-06 13:26:53 +0100

    feat:

---
+++ packages/react-ui/src/stories/ConnectWallet.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import ConnectWallet from "../components/connectWallet";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Visual Components/ConnectWallet",
+  component: ConnectWallet
+} as ComponentMeta<typeof ConnectWallet>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ConnectWallet> = () => <ConnectWallet />;
+
+export const Primary: ComponentStory<typeof ConnectWallet> = Template.bind({});
