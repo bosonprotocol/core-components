b952d427379e874dc0f63e5ccf7dc18849e3ff68
Roberto Sousa <beto.sousa22@gmail.com>
2022-07-06 01:30:49 +0100

    refactor: remove components
    not used components lib

--- packages/visual-components/src/lib/components/details/shared-utils.tsx
+++
@@ -1,8 +0,0 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-
-dayjs.extend(utc);
-
-export function formatDate(date: string) {
-  return dayjs(parseInt(date)).utc().format("DD/MM/YYYY HH:mm:ss UTC");
-}
