724aeb71eef02d38d9e89cd1c9ca8ac5a6876594
Roberto Sousa <beto.sousa22@gmail.com>
2022-07-14 00:43:36 +0100

    feat: build redeem cta

---
+++ packages/react-kit/src/components/cta/exchange/RedeemButton.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { BigNumberish, providers } from "ethers";
+
+import { Button, ButtonSize } from "../../buttons/Button";
+import { useCoreSdk } from "../../../hooks/useCoreSdk";
+import { useSignerAddress } from "../../../hooks/useSignerAddress";
+import { useMetaTxHandlerContract } from "../../../hooks/meta-tx/useMetaTxHandlerContract";
+import { ButtonTextWrapper, ExtraInfo, LoadingWrapper } from "../common/styles";
+import { CtaButtonProps } from "../common/types";
+import { Loading } from "../../Loading";
+
+type Props = { exchangeId: BigNumberish } & CtaButtonProps<{
+  exchangeId: BigNumberish;
+}>;
+
+export const RedeemButton = ({
+  exchangeId,
+  metaTransactionsApiKey,
+  disabled = false,
+  extraInfo,
+  onSuccess,
+  onError,
+  onPendingSignature,
+  onPendingTransaction,
+  waitBlocks = 1,
+  children,
+  size = ButtonSize.Large,
+  ...coreSdkConfig
+}: Props) => {
+  const coreSdk = useCoreSdk(coreSdkConfig);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signerAddress = useSignerAddress(coreSdkConfig.web3Provider);
+  const metaTxContract = useMetaTxHandlerContract({
+    chainId: coreSdkConfig.chainId,
+    metaTransactionsApiKey,
+    web3Provider: coreSdkConfig.web3Provider
+  });
+
+  return (
+    <Button
+      variant="primary"
+      size={size}
+      disabled={disabled}
+      onClick={async () => {
+        if (!isLoading) {
+          try {
+            setIsLoading(true);
+            onPendingSignature?.();
+
+            let txResponse;
+
+            if (metaTransactionsApiKey && metaTxContract && signerAddress) {
+              const nonce = Date.now();
+
+              const { r, s, v } = await coreSdk.signExecuteMetaTxRedeemVoucher({
+                chainId: coreSdkConfig.chainId,
+                exchangeId,
+                nonce
+              });
+
+              txResponse = await metaTxContract.executeMetaTxRedeemVoucher(
+                signerAddress,
+                {
+                  exchangeId
+                },
+                nonce,
+                r,
+                s,
+                v
+              );
+            } else {
+              txResponse = await coreSdk.redeemVoucher(exchangeId);
+            }
+
+            onPendingTransaction?.(txResponse.hash);
+            const receipt = await txResponse.wait(waitBlocks);
+
+            onSuccess?.(receipt as providers.TransactionReceipt, {
+              exchangeId
+            });
+          } catch (error) {
+            onError?.(error as Error);
+          } finally {
+            setIsLoading(false);
+          }
+        }
+      }}
+    >
+      <ButtonTextWrapper>
+        {children || "Redeem"}
+        {extraInfo && !isLoading && <ExtraInfo>{extraInfo}</ExtraInfo>}
+        {extraInfo && isLoading && (
+          <LoadingWrapper>
+            <Loading />
+          </LoadingWrapper>
+        )}
+      </ButtonTextWrapper>
+    </Button>
+  );
+};
