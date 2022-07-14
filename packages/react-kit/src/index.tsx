650fb9590e239a70d89625226f23989a4a9886e4
Rafał Mikucki <rafalmikucki@gmail.com>
2022-07-14 14:39:02 +0200

    fix: update imports

--- packages/react-kit/src/index.tsx
+++ packages/react-kit/src/index.tsx
@@ -5,6 +5,7 @@ export * from "@bosonprotocol/ipfs-storage";
 export * from "./components/Loading";
 export * from "./components/cta/exchange/CancelButton";
 export * from "./components/cta/exchange/RevokeButton";
+export * from "./components/cta/exchange/RedeemButton";
 export * from "./components/cta/offer/CommitButton";
 export * from "./components/cta/offer/VoidButton";
 export * from "./components/buttons/Button";
