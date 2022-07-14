b046a4f5ff2c287155207744fb7a5f64cc7521bb
RobertoMSousa <beto.sousa22@gmail.com>
2022-07-14 10:43:14 +0100

    feat: build redeem cta (#162)

--- packages/react-kit/src/components/cta/common/styles.ts
+++ packages/react-kit/src/components/cta/common/styles.ts
@@ -8,7 +8,7 @@ export const ExtraInfo = styled.span`
   font-weight: 600;
   font-size: 0.75rem;
   line-height: 150%;
-  margin-left: 3.75rem;
+  margin-left: 3.125rem;
   color: ${({ theme }) => theme?.colors?.light.black};
   opacity: 0.5;
 `;
---
+++ packages/react-kit/src/stories/cta/exchange/RedeemButton.stories.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { RedeemButton } from "../../../components/cta/exchange/RedeemButton";
+import { hooks } from "../../helpers/connect-wallet";
+import { CtaButtonWrapper } from "../../helpers/CtaButtonWrapper";
+
+export default {
+  title: "Visual Components/CTA/Exchange/RedeemButton",
+  component: RedeemButton
+} as ComponentMeta<typeof RedeemButton>;
+
+const Template: ComponentStory<typeof RedeemButton> = (args) => {
+  const provider = hooks.useProvider();
+
+  return (
+    <CtaButtonWrapper>
+      <RedeemButton web3Provider={provider} {...args} />
+    </CtaButtonWrapper>
+  );
+};
+
+export const Simple: ComponentStory<typeof RedeemButton> = Template.bind({});
+export const WithExtraInfo: ComponentStory<typeof RedeemButton> = Template.bind(
+  {}
+);
+
+Simple.args = {
+  chainId: 1234,
+  exchangeId: "92",
+  disabled: false,
+  web3Provider: undefined,
+  metaTransactionsApiKey: undefined,
+  onPendingSignature: () => {
+    console.log("----------ON PENDING SIGNATURE-------------");
+  },
+  onPendingTransaction: (txHash: string) => {
+    console.log("----------ON PENDING TRANSACTION-------------");
+    console.log("txHash", txHash);
+  },
+  onSuccess: (receipt, payload) => {
+    console.log("----------ON SUCCESS-------------");
+    console.log("receipt", receipt);
+    console.log("payload", payload);
+  },
+  onError: (error) => {
+    console.log("----------ON ERROR-------------");
+    console.log("error", error);
+  }
+};
+
+WithExtraInfo.args = {
+  chainId: 1234,
+  exchangeId: "28",
+  disabled: false,
+  web3Provider: undefined,
+  metaTransactionsApiKey: undefined,
+  extraInfo: "Step 2",
+  onPendingSignature: () => {
+    console.log("----------ON PENDING SIGNATURE-------------");
+  },
+  onPendingTransaction: (txHash: string) => {
+    console.log("----------ON PENDING TRANSACTION-------------");
+    console.log("txHash", txHash);
+  },
+  onSuccess: (receipt, payload) => {
+    console.log("----------ON SUCCESS-------------");
+    console.log("receipt", receipt);
+    console.log("payload", payload);
+  },
+  onError: (error) => {
+    console.log("----------ON ERROR-------------");
+    console.log("error", error);
+  }
+};
