cce723d3d0c403ea563a744a9a4e8ce6d6b94a91
Roberto Sousa <beto.sousa22@gmail.com>
2022-07-21 09:45:26 +0100

    feat: conclude search bar

--- packages/react-kit/src/components/searchBar/SearchBar.tsx
+++ packages/react-kit/src/components/searchBar/SearchBar.tsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { MagnifyingGlass } from "../../icons/MagnifyingGlass";
 import { useCoreSdk } from "../../hooks/useCoreSdk";
+import { InputField, InputWrapper } from "./SearchBar.styles";
+import { BaseMetadataEntityFieldsFragment } from "@bosonprotocol/core-sdk/dist/cjs/subgraph";
 
 interface SearchBarProps {
   placeholder?: string;
   disabled?: boolean;
+  searchResults: BaseMetadataEntityFieldsFragment[];
+  chainId?: number;
 }
 
 export const SearchBar = ({
   placeholder = "Search...",
-  disabled = false
+  disabled = false,
+  searchResults,
+  chainId = 1234
 }: SearchBarProps) => {
   const [value, setValue] = useState("");
 
-  const coreSdk = useCoreSdk({ chainId: 1234 });
+  const coreSdk = useCoreSdk({ chainId });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -26,12 +31,18 @@ export const SearchBar = ({
     if (e.key === "Enter") {
       e.preventDefault();
       console.log(`search with =>${value}`);
-      const sample = await coreSdk.getOffers({ name_contains_nocase: value });
+
+      searchResults = await coreSdk.getBaseMetadataEntities({
+        metadataFilter: {
+          description_contains_nocase: value,
+          name_contains_nocase: value
+        }
+      });
     }
   };
 
   return (
-    <InputWrapper>
+    <InputWrapper disabled={disabled}>
       <MagnifyingGlass />
       <InputField
         value={value}
@@ -39,40 +50,9 @@ export const SearchBar = ({
         type="text"
         placeholder={placeholder}
         onKeyDown={handleEnter}
-        onChange={(e) => {
-          console.log("🚀 ~ file: SearchBar.tsx ~ line 41 ~ e", e);
-          handleChange(e);
-        }}
+        onChange={handleChange}
         disabled={disabled}
       />
     </InputWrapper>
   );
 };
-
-const InputWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0.5rem 1rem;
-  gap: 1rem;
-  height: 2.5rem;
-  left: 1.25rem;
-  background: ${({ theme }) => theme?.colors?.light.lightGrey};
-`;
-
-const InputField = styled.input`
-  border: none;
-  height: 1.563rem;
-  width: 100%;
-  background-color: ${({ theme }) => theme?.colors?.light.lightGrey};
-  font-size: 1rem;
-  line-height: 150%;
-  vertical-align: middle;
-  :focus {
-    outline: none;
-    color: ${({ theme }) => theme?.colors?.light.secondary};
-  }
-  :focus::placeholder {
-    color: transparent;
-  }
-`;
