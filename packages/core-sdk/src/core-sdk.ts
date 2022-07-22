37d755f65fc7015684b5b4ff24f28cb71a7588be
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-07-22 17:44:24 +0200

    docs(core-sdk): top-level ts-doc api annotations (#172)
    * docs(core-sdk): top-level ts-doc api annotations
    
    * test: fix subgraph accounts
    
    * docs: change example chain id to polygon mainnet
    
    * docs: reword storeMetadata docs

--- packages/core-sdk/src/core-sdk.ts
+++ packages/core-sdk/src/core-sdk.ts
@@ -29,6 +29,10 @@ export class CoreSDK {
   private _subgraphUrl: string;
   private _protocolDiamond: string;
 
+  /**
+   * Creates an instance of `CoreSDK`
+   * @param args - Constructor args
+   */
   constructor(opts: {
     web3Lib: Web3LibAdapter;
     subgraphUrl: string;
@@ -43,6 +47,22 @@ export class CoreSDK {
     this._theGraphStorage = opts.theGraphStorage;
   }
 
+  /**
+   * Creates an instance of `CoreSDK` by using default values derived either from
+   * `args.envName` or `args.chainId`.
+   *
+   * @example
+   * Instance which uses the default contract address and subgraph url of mainnet:
+   * ```ts
+   * const coreSdk = CoreSDK.fromDefaultConfig({
+   *   ...otherArgs,
+   *   chainId: 137
+   * })
+   * ```
+   *
+   * @param args - Constructor args.
+   * @returns CoreSDK instance with default values.
+   */
   static fromDefaultConfig(args: {
     web3Lib: Web3LibAdapter;
     envName?: string;
@@ -64,6 +84,14 @@ export class CoreSDK {
     });
   }
 
+  /** Metadata related methods */
+
+  /**
+   * Stores supported offer metadata via the MetadataStorage instance which was passed in
+   * at construction.
+   * @param metadata - Offer metadata of type `BASE` or `PRODUCT_V1`.
+   * @returns Metadata hash / identifier.
+   */
   public async storeMetadata(metadata: AnyMetadata): Promise<string> {
     if (!this._metadataStorage) {
       throw new Error("No metadata storage set");
@@ -72,6 +100,12 @@ export class CoreSDK {
     return this._metadataStorage.storeMetadata(metadata);
   }
 
+  /**
+   * Returns supported offer metadata from passed in `MetadataStorage` instance.
+   * @param metadataHashOrUri - Metadata hash or uri that can be handled by the
+   * storage instance.
+   * @returns Metadata hash / identifier.
+   */
   public async getMetadata(metadataHashOrUri: string): Promise<AnyMetadata> {
     if (!this._metadataStorage) {
       throw new Error("No metadata storage set");
@@ -80,6 +114,11 @@ export class CoreSDK {
     return this._metadataStorage.getMetadata(metadataHashOrUri);
   }
 
+  /**
+   * Returns `BASE` type offer metadata entities from subgraph.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns BaseMetadataEntities from subgraph.
+   */
   public async getBaseMetadataEntities(
     queryVars?: subgraph.GetBaseMetadataEntitiesQueryQueryVariables
   ): Promise<subgraph.BaseMetadataEntityFieldsFragment[]> {
@@ -89,6 +128,11 @@ export class CoreSDK {
     );
   }
 
+  /**
+   * Returns `PRODUCT_V1` type offer metadata entities from subgraph.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns ProductV1MetadataEntities from subgraph.
+   */
   public async getProductV1MetadataEntities(
     queryVars?: subgraph.GetProductV1MetadataEntitiesQueryQueryVariables
   ): Promise<subgraph.ProductV1MetadataEntityFieldsFragment[]> {
@@ -98,6 +142,14 @@ export class CoreSDK {
     );
   }
 
+  /** Account related methods */
+
+  /**
+   * Returns seller entity from subgraph.
+   * @param sellerId - ID of seller entity to query for.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Seller entity from subgraph.
+   */
   public async getSellerById(
     sellerId: BigNumberish,
     queryVars?: accounts.subgraph.SingleSellerQueryVariables
@@ -109,6 +161,12 @@ export class CoreSDK {
     );
   }
 
+  /**
+   * Returns seller entity from subgraph.
+   * @param operator - Operator address of seller entity to query for.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Seller entity from subgraph.
+   */
   public async getSellerByOperator(
     operator: string,
     queryVars?: subgraph.GetSellersQueryQueryVariables
@@ -120,6 +178,12 @@ export class CoreSDK {
     );
   }
 
+  /**
+   * Returns seller entity from subgraph.
+   * @param clerk - Clerk address of seller entity to query for.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Seller entity from subgraph.
+   */
   public async getSellerByClerk(
     clerk: string,
     queryVars?: subgraph.GetSellersQueryQueryVariables
@@ -131,6 +195,12 @@ export class CoreSDK {
     );
   }
 
+  /**
+   * Returns seller entity from subgraph. Matches `operator`, `clerk`, `admin` or `treasury`.
+   * @param address - Address of seller entity to query for.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Seller entity from subgraph.
+   */
   public async getSellerByAddress(
     address: string,
     queryVars?: subgraph.GetSellersQueryQueryVariables
@@ -142,12 +212,23 @@ export class CoreSDK {
     );
   }
 
+  /**
+   * Returns seller entities from subgraph.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Seller entities from subgraph.
+   */
   public async getSellers(
     queryVars?: subgraph.GetSellersQueryQueryVariables
   ): Promise<subgraph.SellerFieldsFragment[]> {
     return accounts.subgraph.getSellers(this._subgraphUrl, queryVars);
   }
 
+  /**
+   * Creates seller account by calling the `AccountHandlerFacet` contract.
+   * @param sellerToCreate - Addresses to set in the seller account.
+   * @param overrides - Optional overrides.
+   * @returns Transaction response.
+   */
   public async createSeller(
     sellerToCreate: accounts.CreateSellerArgs,
     overrides: Partial<{
@@ -161,6 +242,14 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Creates seller account and offer by calling the `OrchestrationHandlerFacet` contract.
+   * This transaction only succeeds if there is no existing seller account for the connected signer.
+   * @param sellerToCreate - Addresses to set in the seller account.
+   * @param offerToCreate - Offer arguments.
+   * @param overrides - Optional overrides.
+   * @returns Transaction response.
+   */
   public async createSellerAndOffer(
     sellerToCreate: accounts.CreateSellerArgs,
     offerToCreate: offers.CreateOfferArgs,
@@ -178,6 +267,15 @@ export class CoreSDK {
     });
   }
 
+  /** Offer related methods */
+
+  /**
+   * Creates offer by calling the `OfferHandlerFacet` contract.
+   * This transaction only succeeds if there is an existing seller account for connected signer.
+   * @param offerToCreate - Offer arguments.
+   * @param overrides - Optional overrides.
+   * @returns Transaction response.
+   */
   public async createOffer(
     offerToCreate: offers.CreateOfferArgs,
     overrides: Partial<{
@@ -193,6 +291,12 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Utility method to retrieve the created `offerId` from logs after calling `createOffer`
+   * or `createOfferAndSeller`.
+   * @param logs - Logs to search in.
+   * @returns Created offer id.
+   */
   public getCreatedOfferIdFromLogs(logs: Log[]): string | null {
     const offerId = getValueFromLogs({
       iface: offers.iface.bosonOfferHandlerIface,
@@ -212,6 +316,13 @@ export class CoreSDK {
     );
   }
 
+  /**
+   * Voids an existing offer by calling the `OfferHandlerFacet` contract.
+   * This transaction only succeeds if the connected signer is the `operator`.
+   * @param offerId - ID of offer to void.
+   * @param overrides - Optional overrides.
+   * @returns Transaction response.
+   */
   public async voidOffer(
     offerId: BigNumberish,
     overrides: Partial<{
@@ -226,6 +337,12 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Returns offer from subgraph.
+   * @param offerId - ID of offer.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Offer entity from subgraph.
+   */
   public async getOfferById(
     offerId: BigNumberish,
     queryVars?: offers.subgraph.SingleOfferQueryVariables
@@ -233,36 +350,25 @@ export class CoreSDK {
     return offers.subgraph.getOfferById(this._subgraphUrl, offerId, queryVars);
   }
 
+  /**
+   * Returns offers from subgraph.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Offer entities from subgraph.
+   */
   public async getOffers(
     queryVars?: subgraph.GetOffersQueryQueryVariables
   ): Promise<subgraph.OfferFieldsFragment[]> {
     return offers.subgraph.getOffers(this._subgraphUrl, queryVars);
   }
 
-  public async commitToOffer(
-    offerId: BigNumberish,
-    overrides: Partial<{
-      buyer: string;
-    }> = {}
-  ): Promise<TransactionResponse> {
-    return exchanges.handler.commitToOffer({
-      buyer: overrides.buyer || (await this._web3Lib.getSignerAddress()),
-      offerId,
-      web3Lib: this._web3Lib,
-      subgraphUrl: this._subgraphUrl,
-      contractAddress: this._protocolDiamond
-    });
-  }
+  /** ERC20 / Exchange Token related methods */
 
-  public getCommittedExchangeIdFromLogs(logs: Log[]): string | null {
-    return getValueFromLogs({
-      iface: exchanges.iface.bosonExchangeHandlerIface,
-      logs,
-      eventArgsKey: "exchangeId",
-      eventName: "BuyerCommitted"
-    });
-  }
-
+  /**
+   * Returns the current allowance of the given token by calling the contract.
+   * @param exchangeToken - Address of exchange token.
+   * @param overrides - Optional overrides.
+   * @returns Allowance for given signer.
+   */
   public async getExchangeTokenAllowance(
     exchangeToken: string,
     overrides: Partial<{
@@ -278,6 +384,11 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Returns `name`, `decimals` and `symbol` of the given token by calling the contract.
+   * @param exchangeToken - Address exchange token.
+   * @returns Decimals, name and symbol.
+   */
   public async getExchangeTokenInfo(exchangeToken: string): Promise<{
     name: string;
     decimals: number;
@@ -296,6 +407,13 @@ export class CoreSDK {
     return { decimals, name, symbol };
   }
 
+  /**
+   * Approves the given amount for the main protocol contract.
+   * @param exchangeToken - Address of token to approve.
+   * @param value - Amount of allowance.
+   * @param overrides - Optional overrides.
+   * @returns Transaction response.
+   */
   public async approveExchangeToken(
     exchangeToken: string,
     value: BigNumberish,
@@ -311,6 +429,15 @@ export class CoreSDK {
     });
   }
 
+  /** Funds related methods */
+
+  /**
+   * Deposit funds by calling the `FundsHandlerFacet` contract.
+   * @param sellerId - ID of seller account to deposit funds for.
+   * @param fundsAmount - Amount of funds.
+   * @param fundsTokenAddress - Address of funds token.
+   * @returns Transaction response.
+   */
   public async depositFunds(
     sellerId: BigNumberish,
     fundsAmount: BigNumberish,
@@ -325,6 +452,12 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Returns funds entity from subgraph.
+   * @param fundsId - ID of funds entity.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Funds entity from subgraph.
+   */
   public async getFundsById(
     fundsId: BigNumberish,
     queryVars?: subgraph.GetFundsByIdQueryVariables
@@ -332,12 +465,24 @@ export class CoreSDK {
     return funds.subgraph.getFundsById(this._subgraphUrl, fundsId, queryVars);
   }
 
+  /**
+   * Returns funds entities from subgraph.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Funds entities from subgraph.
+   */
   public async getFunds(
     queryVars?: subgraph.GetFundsQueryVariables
   ): Promise<subgraph.FundsEntityFieldsFragment[]> {
     return funds.subgraph.getFunds(this._subgraphUrl, queryVars);
   }
 
+  /**
+   * Withdraw selected funds by calling the `FundsHandlerFacet` contract.
+   * @param sellerId - ID of seller account to withdraw funds for.
+   * @param tokensToWithdraw - Addresses of funds tokens to withdraw.
+   * @param amountsToWithdraw - Amounts of funds token to withdraw.
+   * @returns Transaction response.
+   */
   public async withdrawFunds(
     sellerId: BigNumberish,
     tokensToWithdraw: Array<string>,
@@ -352,6 +497,11 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Withdraw all available funds by calling the `FundsHandlerFacet` contract.
+   * @param sellerId - ID of seller account to withdraw funds for.
+   * @returns Transaction response.
+   */
   public async withdrawAllAvailableFunds(
     sellerId: BigNumberish
   ): Promise<TransactionResponse> {
@@ -363,6 +513,14 @@ export class CoreSDK {
     });
   }
 
+  /** Exchange related methods */
+
+  /**
+   * Returns exchange entity from subgraph.
+   * @param exchangeId - ID of exchange entity.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Exchange entity from subgraph.
+   */
   public async getExchangeById(
     exchangeId: BigNumberish,
     queryVars?: subgraph.GetExchangeByIdQueryQueryVariables
@@ -374,12 +532,59 @@ export class CoreSDK {
     );
   }
 
+  /**
+   * Returns exchange entities from subgraph.
+   * @param queryVars - Optional query variables to skip, order or filter.
+   * @returns Exchange entities from subgraph.
+   */
   public async getExchanges(
     queryVars?: subgraph.GetExchangesQueryQueryVariables
   ): Promise<subgraph.ExchangeFieldsFragment[]> {
     return exchanges.subgraph.getExchanges(this._subgraphUrl, queryVars);
   }
 
+  /**
+   * Commits to an offer by calling the `ExchangeHandlerContract`.
+   * This transaction only succeeds if the seller has deposited funds.
+   * @param offerId - ID of offer to commit to.
+   * @param overrides - Optional overrides.
+   * @returns Transaction response.
+   */
+  public async commitToOffer(
+    offerId: BigNumberish,
+    overrides: Partial<{
+      buyer: string;
+    }> = {}
+  ): Promise<TransactionResponse> {
+    return exchanges.handler.commitToOffer({
+      buyer: overrides.buyer || (await this._web3Lib.getSignerAddress()),
+      offerId,
+      web3Lib: this._web3Lib,
+      subgraphUrl: this._subgraphUrl,
+      contractAddress: this._protocolDiamond
+    });
+  }
+
+  /**
+   * Utility method to retrieve the created `exchangeId` from logs after calling `commitToOffer`.
+   * @param logs - Logs to search in.
+   * @returns Created exchange id.
+   */
+  public getCommittedExchangeIdFromLogs(logs: Log[]): string | null {
+    return getValueFromLogs({
+      iface: exchanges.iface.bosonExchangeHandlerIface,
+      logs,
+      eventArgsKey: "exchangeId",
+      eventName: "BuyerCommitted"
+    });
+  }
+
+  /**
+   * Revokes an existing voucher by calling the `ExchangeHandlerContract`.
+   * Callable by seller `operator`.
+   * @param exchangeId - ID of exchange to revoke.
+   * @returns Transaction response.
+   */
   public async revokeVoucher(
     exchangeId: BigNumberish
   ): Promise<TransactionResponse> {
@@ -391,6 +596,12 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Cancels an existing voucher by calling the `ExchangeHandlerContract`.
+   * Callable by buyer.
+   * @param exchangeId - ID of exchange to cancel.
+   * @returns Transaction response.
+   */
   public async cancelVoucher(
     exchangeId: BigNumberish
   ): Promise<TransactionResponse> {
@@ -402,6 +613,12 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Redeems an existing voucher by calling the `ExchangeHandlerContract`.
+   * Callable by buyer.
+   * @param exchangeId - ID of exchange to redeem.
+   * @returns Transaction response.
+   */
   public async redeemVoucher(
     exchangeId: BigNumberish
   ): Promise<TransactionResponse> {
@@ -413,6 +630,12 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Completes an existing voucher by calling the `ExchangeHandlerContract`.
+   * Callable by buyer or seller operator.
+   * @param exchangeId - ID of exchange to complete.
+   * @returns Transaction response.
+   */
   public async completeExchange(
     exchangeId: BigNumberish
   ): Promise<TransactionResponse> {
@@ -424,6 +647,11 @@ export class CoreSDK {
     });
   }
 
+  /**
+   * Expires an existing voucher by calling the `ExchangeHandlerContract`.
+   * @param exchangeId - ID of exchange to expire.
+   * @returns Transaction response.
+   */
   public async expireVoucher(
     exchangeId: BigNumberish
   ): Promise<TransactionResponse> {
@@ -435,6 +663,13 @@ export class CoreSDK {
     });
   }
 
+  /** Meta Tx related methods */
+
+  /**
+   * Encodes and signs a meta transaction that can be relayed.
+   * @param args - Meta transaction args.
+   * @returns Signature.
+   */
   public async signExecuteMetaTx(
     args: Omit<
       Parameters<typeof metaTx.handler.signExecuteMetaTx>[0],
