5b8e44ec02861f19f6ee1345b3cf6b0f7b22d875
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2022-08-25 08:43:28 +0100

    fix contracts deployment

--- packages/common/src/configs.ts
+++ packages/common/src/configs.ts
@@ -49,8 +49,8 @@ export const defaultConfigs: ProtocolConfig[] = [
     ipfsMetadataUrl: "http://127.0.0.1:5001",
     widgetsUrl: "http://127.0.0.1:3000",
     contracts: {
-      protocolDiamond: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
-      testErc20: "0x4c5859f0F772848b2D91F1D83E2Fe57935348029"
+      protocolDiamond: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
+      testErc20: "0x5FbDB2315678afecb367f032d93F642f64180aa3"
     }
   }
 ];
