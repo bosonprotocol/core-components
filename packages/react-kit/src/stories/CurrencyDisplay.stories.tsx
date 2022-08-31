2b82634e5b1b2e98129b79f79d4ecaafca372af7
Roberto Sousa <beto.sousa22@gmail.com>
2022-08-31 15:08:19 +0100

    build: :art: build the story

---
+++ packages/react-kit/src/stories/CurrencyDisplay.stories.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { CurrencyDisplay } from "../components/currencyDisplay/CurrencyDisplay";
+import { Currencies } from "../types/Currencies";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Visual Components/CurrencyDisplay",
+  component: CurrencyDisplay
+} as ComponentMeta<typeof CurrencyDisplay>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof CurrencyDisplay> = (args) => (
+  <CurrencyDisplay {...args} />
+);
+
+export const CurrencyDisplayWithPrice: ComponentStory<typeof CurrencyDisplay> =
+  Template.bind({});
+
+export const CurrencyDisplaySmall: ComponentStory<typeof CurrencyDisplay> =
+  Template.bind({});
+
+export const CurrencyDisplayLarge: ComponentStory<typeof CurrencyDisplay> =
+  Template.bind({});
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+CurrencyDisplayWithPrice.args = {
+  currency: Currencies.ETH,
+  height: 25,
+  value: 10
+};
+
+CurrencyDisplaySmall.args = {
+  currency: Currencies.ETH,
+  height: 25,
+  value: undefined
+};
+
+CurrencyDisplayLarge.args = {
+  currency: Currencies.ETH,
+  height: 100,
+  value: undefined
+};
