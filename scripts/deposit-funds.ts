1a007ff859f39fba523de8b8ce7ba5cfa98c1eea
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2022-09-09 09:12:32 +0100

    Update scripts/deposit-funds.ts
    Co-authored-by: albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>

--- scripts/deposit-funds.ts
+++ scripts/deposit-funds.ts
@@ -13,7 +13,6 @@ function validAddress(value, previous) {
 }
 
 function validInt(value) {
-  // parseInt takes a string and a radix
   const parsedValue = parseInt(value, 10);
   if (isNaN(parsedValue)) {
     throw new InvalidArgumentError("Not a number.");
