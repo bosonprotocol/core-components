cee9e8b9e0adbd35c31ec4e626eb7f8cf9d74b46
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2022-09-16 14:26:47 +0100

    fix: fix hook useIpfsMetadataStorage

--- packages/react-kit/src/hooks/useIpfsMetadataStorage.tsx
+++ packages/react-kit/src/hooks/useIpfsMetadataStorage.tsx
@@ -6,41 +6,35 @@ import { EnvironmentType } from "@bosonprotocol/common/src/types";
 /**
  * Hook that initializes an instance of `IpfsMetadataStorage` from the `@bosonprotocol/ipfs-storage`
  * package.
- * @param config - envName to use default IPFS url or custom url.
+ * @param envName - envName to use default IPFS url.
+ * @param url - optional custom url.
  * @param headers - Optional IPFS http client headers.
  * @returns Instance of `IpfsMetadataStorage`.
  */
 export function useIpfsMetadataStorage(
-  config: {
-    envName?: EnvironmentType;
-    url?: string;
-  },
+  envName: EnvironmentType,
+  url?: string,
   headers?: Headers | Record<string, string>
 ) {
-  const [ipfsMetadataStorage, setIpfsMetadataStorage] = useState<
-    IpfsMetadataStorage | undefined
-  >(initIpfsMetadataStorage(config, headers));
+  const [ipfsMetadataStorage, setIpfsMetadataStorage] =
+    useState<IpfsMetadataStorage>(
+      initIpfsMetadataStorage(envName, url, headers)
+    );
 
   useEffect(() => {
-    setIpfsMetadataStorage(initIpfsMetadataStorage(config, headers));
-  }, [config, headers]);
+    setIpfsMetadataStorage(initIpfsMetadataStorage(envName, url, headers));
+  }, [envName, url, headers]);
 
   return ipfsMetadataStorage;
 }
 
 function initIpfsMetadataStorage(
-  config: {
-    envName?: EnvironmentType;
-    url?: string;
-  },
+  envName: EnvironmentType,
+  url?: string,
   headers?: Headers | Record<string, string>
 ) {
-  if (!config.envName && !config.url) {
-    return undefined;
-  }
-  const url = config.envName
-    ? getDefaultConfig(config.envName).ipfsMetadataUrl
-    : config.url;
-
-  return new IpfsMetadataStorage({ url, headers });
+  return new IpfsMetadataStorage({
+    url: url || getDefaultConfig(envName).ipfsMetadataUrl,
+    headers
+  });
 }
