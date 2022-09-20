38bd192fc1d01026418c30246a63073eaa4fa743
Piotr Kniaź <piotr.kniaz@silksh.pl>
2022-09-20 12:58:17 +0200

    feat: complete exchange button add

---
+++ packages/react-kit/src/components/cta/exchange/CompleteButton.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { BigNumberish, providers } from "ethers";
+
+import { Button, ButtonSize } from "../../buttons/Button";
+import { useCoreSdk } from "../../../hooks/useCoreSdk";
+import { ButtonTextWrapper, ExtraInfo, LoadingWrapper } from "../common/styles";
+import { CtaButtonProps } from "../common/types";
+import { Loading } from "../../Loading";
+
+type Props = {
+  /**
+   * ID of exchange to complete.
+   */
+  exchangeId: BigNumberish;
+} & CtaButtonProps<{
+  exchangeId: BigNumberish;
+}>;
+
+export const CompleteButton = ({
+  exchangeId,
+  metaTransactionsApiKey,
+  disabled = false,
+  extraInfo,
+  onSuccess,
+  onError,
+  onPendingSignature,
+  onPendingTransaction,
+  waitBlocks = 1,
+  size = ButtonSize.Large,
+  variant = "primary",
+  children,
+  ...coreSdkConfig
+}: Props) => {
+  const coreSdk = useCoreSdk(coreSdkConfig);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  return (
+    <Button
+      variant={variant}
+      disabled={disabled}
+      size={size}
+      onClick={async () => {
+        if (!isLoading) {
+          try {
+            setIsLoading(true);
+            onPendingSignature?.();
+            const txResponse = await coreSdk.completeExchange(exchangeId);
+
+            onPendingTransaction?.(txResponse.hash);
+            const receipt = await txResponse.wait(waitBlocks);
+
+            onSuccess?.(receipt as providers.TransactionReceipt, {
+              exchangeId
+            });
+          } catch (error) {
+            onError?.(error as Error);
+          } finally {
+            setIsLoading(false);
+          }
+        }
+      }}
+    >
+      <ButtonTextWrapper>
+        {children || "Complete"}
+        {extraInfo && !isLoading && <ExtraInfo>{extraInfo}</ExtraInfo>}
+        {extraInfo && isLoading && (
+          <LoadingWrapper>
+            <Loading />
+          </LoadingWrapper>
+        )}
+      </ButtonTextWrapper>
+    </Button>
+  );
+};
