6211f757274b5477a0f01281283e0ab716a037a5
Albert Folch <albertfolch@redeemeum.com>
2022-09-22 14:37:15 +0200

    feat: add explorer url getter to retrieve any tx details

--- packages/common/src/types/configs.ts
+++ packages/common/src/types/configs.ts
@@ -19,6 +19,7 @@ export type ProtocolConfig = {
     name: string;
     decimals: string;
   };
+  getTxExplorerUrl: (txHash?: string) => string;
   subgraphUrl: string;
   theGraphIpfsUrl?: string;
   jsonRpcUrl: string;
