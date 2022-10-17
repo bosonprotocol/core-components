92d9c81a67302193c6f7b0964be296ae12ac7841
max-koz <maksymilian.kozak@arianelabs.com>
2022-10-17 20:40:16 +0200

    feat: Add missing CTAs

---
+++ packages/react-kit/src/components/cta/dispute/RemoveFeesDisputeResolver.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { BigNumberish, providers } from "ethers";
+
+import { Button, ButtonSize } from "../../buttons/Button";
+import { useCoreSdk } from "../../../hooks/useCoreSdk";
+import { ButtonTextWrapper, ExtraInfo, LoadingWrapper } from "../common/styles";
+import { CtaButtonProps } from "../common/types";
+import { Loading } from "../../Loading";
+import { CreateSellerArgs } from "@bosonprotocol/common";
+export type ICancelButton = {
+  /**
+   * ID of voucher/exchange to cancel.
+   */
+  exchangeId: BigNumberish;
+  createSellerArgs: CreateSellerArgs;
+  buyerPercent: string;
+  disputeResolverId: BigNumberish;
+  feeTokenAddresses: string[];
+} & CtaButtonProps<{
+  exchangeId: BigNumberish;
+}>;
+
+export const CancelButton = ({
+  exchangeId,
+  disabled = false,
+  showLoading = false,
+  extraInfo,
+  onSuccess,
+  onError,
+  onPendingSignature,
+  onPendingTransaction,
+  waitBlocks = 1,
+  children,
+  size = ButtonSize.Large,
+  variant = "secondary",
+  createSellerArgs,
+  buyerPercent,
+  disputeResolverId,
+  feeTokenAddresses,
+  ...coreSdkConfig
+}: ICancelButton) => {
+  const coreSdk = useCoreSdk(coreSdkConfig);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  return (
+    <Button
+      variant={variant}
+      size={size}
+      disabled={disabled}
+      onClick={async (e) => {
+        e.stopPropagation();
+        if (!isLoading) {
+          try {
+            setIsLoading(true);
+            onPendingSignature?.();
+
+            const txResponse = await coreSdk.removeFeesFromDisputeResolver(
+              disputeResolverId,
+              feeTokenAddresses
+            );
+
+            onPendingTransaction?.(txResponse.hash);
+            const receipt = await txResponse.wait(waitBlocks);
+
+            onSuccess?.(receipt as providers.TransactionReceipt, {
+              exchangeId
+            });
+          } catch (error) {
+            onError?.(error as Error);
+          } finally {
+            setIsLoading(false);
+          }
+        }
+      }}
+    >
+      <ButtonTextWrapper>
+        {children || "Create Seller"}
+        {extraInfo && ((!isLoading && showLoading) || !showLoading) ? (
+          <ExtraInfo>{extraInfo}</ExtraInfo>
+        ) : (
+          <>
+            {isLoading && showLoading && (
+              <LoadingWrapper>
+                <Loading />
+              </LoadingWrapper>
+            )}
+          </>
+        )}
+      </ButtonTextWrapper>
+    </Button>
+  );
+};
---
+++ packages/react-kit/src/components/cta/seller/CreateSellerButton.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { BigNumberish, providers } from "ethers";
+
+import { Button, ButtonSize } from "../../buttons/Button";
+import { useCoreSdk } from "../../../hooks/useCoreSdk";
+import { useSignerAddress } from "../../../hooks/useSignerAddress";
+import { ButtonTextWrapper, ExtraInfo, LoadingWrapper } from "../common/styles";
+import { CtaButtonProps } from "../common/types";
+import { Loading } from "../../Loading";
+import { CreateSellerArgs } from "@bosonprotocol/common";
+export type ICancelButton = {
+  /**
+   * ID of voucher/exchange to cancel.
+   */
+  exchangeId: BigNumberish;
+  createSellerArgs: CreateSellerArgs;
+} & CtaButtonProps<{
+  exchangeId: BigNumberish;
+}>;
+
+export const CancelButton = ({
+  exchangeId,
+  disabled = false,
+  showLoading = false,
+  extraInfo,
+  onSuccess,
+  onError,
+  onPendingSignature,
+  onPendingTransaction,
+  waitBlocks = 1,
+  children,
+  size = ButtonSize.Large,
+  variant = "secondary",
+  createSellerArgs,
+  ...coreSdkConfig
+}: ICancelButton) => {
+  const coreSdk = useCoreSdk(coreSdkConfig);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signerAddress = useSignerAddress(coreSdkConfig.web3Provider);
+
+  return (
+    <Button
+      variant={variant}
+      size={size}
+      disabled={disabled}
+      onClick={async (e) => {
+        e.stopPropagation();
+        if (!isLoading) {
+          try {
+            setIsLoading(true);
+            onPendingSignature?.();
+
+            let txResponse;
+
+            if (coreSdk.isMetaTxConfigSet && signerAddress) {
+              const nonce = Date.now();
+
+              const { r, s, v, functionName, functionSignature } =
+                await coreSdk.signMetaTxCreateSeller({
+                  createSellerArgs,
+                  nonce,
+                  chainId: 0
+                });
+
+              txResponse = await coreSdk.relayMetaTransaction({
+                functionName,
+                functionSignature,
+                sigR: r,
+                sigS: s,
+                sigV: v,
+                nonce
+              });
+            } else {
+              txResponse = await coreSdk.createSeller(createSellerArgs);
+            }
+
+            onPendingTransaction?.(txResponse.hash);
+            const receipt = await txResponse.wait(waitBlocks);
+
+            onSuccess?.(receipt as providers.TransactionReceipt, {
+              exchangeId
+            });
+          } catch (error) {
+            onError?.(error as Error);
+          } finally {
+            setIsLoading(false);
+          }
+        }
+      }}
+    >
+      <ButtonTextWrapper>
+        {children || "Create Seller"}
+        {extraInfo && ((!isLoading && showLoading) || !showLoading) ? (
+          <ExtraInfo>{extraInfo}</ExtraInfo>
+        ) : (
+          <>
+            {isLoading && showLoading && (
+              <LoadingWrapper>
+                <Loading />
+              </LoadingWrapper>
+            )}
+          </>
+        )}
+      </ButtonTextWrapper>
+    </Button>
+  );
+};
