8f9ff14815b4a7d9136af95ea87eddc7dd5e45a8
Piotr <30327636+pio-kniaz@users.noreply.github.com>
2022-10-17 11:46:52 +0200

    feat: commit button pas ref add (#315)
    Co-authored-by: Piotr Kniaź <piotr.kniaz@silksh.pl>

--- packages/react-kit/src/components/buttons/Button.tsx
+++ packages/react-kit/src/components/buttons/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Loading } from "../Loading";
 
 import { ButtonStyle } from "./Button.styles";
@@ -29,27 +29,33 @@ export interface ButtonProps {
   children?: React.ReactNode;
 }
 
-export const Button = ({
-  children,
-  onClick,
-  className,
-  size = ButtonSize.Medium,
-  variant = "primary",
-  ...props
-}: ButtonProps) => {
-  return (
-    <ButtonStyle
-      variant={variant}
-      className={className}
-      onClick={onClick}
-      size={size}
-      {...props}
-    >
-      {props.loading ? (
-        <Loading data-loading />
-      ) : (
-        <span id="buttonText">{children}</span>
-      )}
-    </ButtonStyle>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      onClick,
+      className,
+      size = ButtonSize.Medium,
+      variant = "primary",
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <ButtonStyle
+        variant={variant}
+        className={className}
+        onClick={onClick}
+        size={size}
+        ref={ref}
+        {...props}
+      >
+        {props.loading ? (
+          <Loading data-loading />
+        ) : (
+          <span id="buttonText">{children}</span>
+        )}
+      </ButtonStyle>
+    );
+  }
+);
