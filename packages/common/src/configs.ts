a99f4f1693bb19567ae0968891d1efd323b08de4
Roberto Sousa <beto.sousa22@gmail.com>
2022-10-26 11:34:42 +0100

    feat: erc tokens

--- packages/common/src/configs.ts
+++ packages/common/src/configs.ts
@@ -125,7 +125,9 @@ export const defaultConfigs: ProtocolConfig[] = [
     ipfsMetadataUrl: "http://127.0.0.1:5001",
     contracts: {
       protocolDiamond: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
-      testErc20: "0xb7278A61aa25c888815aFC32Ad3cC52fF24fE575" // Foreign20 contract
+      testErc20: "0xb7278A61aa25c888815aFC32Ad3cC52fF24fE575", // Foreign20 contract
+      testErc721: "0xCD8a1C3ba11CF5ECfa6267617243239504a98d90", // Foreign721 contract
+      testErc1155: "0x82e01223d51Eb87e16A03E24687EDF0F294da6f1" // Foreign1155 contract
     },
     metaTx: {
       relayerUrl: "http://localhost:8888"
