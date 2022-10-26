33831850945ccb41a07196ebb84dff9d844d9941
Piotr <30327636+pio-kniaz@users.noreply.github.com>
2022-10-26 11:13:37 +0200

    feat: exchange cta updates (#354)
    Co-authored-by: Piotr Kniaź <piotr.kniaz@silksh.pl>

--- packages/react-kit/src/components/cta/exchange/CancelButton.tsx
+++ packages/react-kit/src/components/cta/exchange/CancelButton.tsx
@@ -26,7 +26,7 @@ export const CancelButton = ({
   waitBlocks = 1,
   children,
   size = ButtonSize.Large,
-  variant = "accentInverted",
+  variant = "secondaryFill",
   ...coreSdkConfig
 }: ICancelButton) => {
   const coreSdk = useCoreSdk(coreSdkConfig);
