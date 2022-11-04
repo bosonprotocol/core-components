3bca4e994b71f1b84dddd0f9e36f475ae2cd0f88
max-koz <109581727+max-koz@users.noreply.github.com>
2022-11-04 10:09:59 +0100

    feat: add funts ctas (#372)
    * feat: implement funds buttons
    
    * fix: remove c-log

---
+++ packages/react-kit/src/stories/cta/funds/WithdrawAllFunds.stories.tsx
@@ -0,0 +1,122 @@
+import React, { useRef } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { WithdrawAllFundsButton } from "../../../components/cta/funds/WithdrawAllFundsButton";
+
+import { hooks } from "../../helpers/connect-wallet";
+import { CtaButtonWrapper } from "../../helpers/CtaButtonWrapper";
+
+export default {
+  title: "Visual Components/CTA/funds/WithdrawAllFundsButton",
+  component: WithdrawAllFundsButton
+} as ComponentMeta<typeof WithdrawAllFundsButton>;
+
+const Template: ComponentStory<typeof WithdrawAllFundsButton> = (args) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const provider = hooks.useProvider();
+
+  return (
+    <CtaButtonWrapper>
+      <WithdrawAllFundsButton
+        web3Provider={provider}
+        {...args}
+        buttonRef={buttonRef}
+      />
+    </CtaButtonWrapper>
+  );
+};
+
+export const Simple: ComponentStory<typeof WithdrawAllFundsButton> =
+  Template.bind({});
+export const WithStep: ComponentStory<typeof WithdrawAllFundsButton> =
+  Template.bind({});
+export const WithMetaTx: ComponentStory<typeof WithdrawAllFundsButton> =
+  Template.bind({});
+
+Simple.args = {
+  envName: "testing",
+  accountId: "42",
+  disabled: false,
+  onPendingSignature: () => {
+    console.log("----------ON PENDING SIGNATURE-------------");
+  },
+  onPendingTransaction: (txHash: string) => {
+    console.log("----------ON PENDING TRANSACTION-------------");
+    console.log("txHash", txHash);
+  },
+  onCancelledTransaction: (oldTxHash, newTxResponse) => {
+    console.log("----------ON CANCELLED TRANSACTION-------------");
+    console.log({ oldTxHash, newTxResponse });
+  },
+  onRepricedTransaction: (oldTxHash, newTxResponse, newTxReceipt) => {
+    console.log("----------ON REPRICED TRANSACTION-------------");
+    console.log({ oldTxHash, newTxResponse, newTxReceipt });
+  },
+  onSuccess: (receipt, payload) => {
+    console.log("----------ON SUCCESS-------------");
+    console.log("receipt", receipt);
+    console.log("payload", payload);
+  },
+  onError: (error) => {
+    console.log("----------ON ERROR-------------");
+    console.log("error", error);
+  }
+};
+
+WithStep.args = {
+  envName: "testing",
+  extraInfo: "Step 1",
+  disabled: false,
+  onPendingSignature: () => {
+    console.log("----------ON PENDING SIGNATURE-------------");
+  },
+  onPendingTransaction: (txHash: string) => {
+    console.log("----------ON PENDING TRANSACTION-------------");
+    console.log("txHash", txHash);
+  },
+  onSuccess: (receipt, payload) => {
+    console.log("----------ON SUCCESS-------------");
+    console.log("receipt", receipt);
+    console.log("payload", payload);
+  },
+  onError: (error) => {
+    console.log("----------ON ERROR-------------");
+    console.log("error", error);
+  }
+};
+
+WithMetaTx.args = {
+  envName: "testing",
+  extraInfo: "",
+  disabled: false,
+  onPendingSignature: () => {
+    console.log("----------ON PENDING SIGNATURE-------------");
+  },
+  onPendingTransaction: (txHash: string) => {
+    console.log("----------ON PENDING TRANSACTION-------------");
+    console.log("txHash", txHash);
+  },
+  onSuccess: (receipt, payload) => {
+    console.log("----------ON SUCCESS-------------");
+    console.log("receipt", receipt);
+    console.log("payload", payload);
+  },
+  onError: (error) => {
+    console.log("----------ON ERROR-------------");
+    console.log("error", error);
+  },
+  metaTx: {
+    relayerUrl: "https://api.biconomy.io",
+    apiIds: {
+      "0x785a225ebac1b600ca3170c6c7fa3488a203fc21": {
+        // BOSON PROTOCOL
+        executeMetaTransaction: "eaeff5a5-2efd-4c2b-85f5-b597c79eabf2"
+      },
+      "0x1f5431e8679630790e8eba3a9b41d1bb4d41aed0": {
+        // BOSON TOKEN
+        executeMetaTransaction: "0cfeee86-a304-4761-a1fd-dcf63ffd153c"
+      }
+    },
+    apiKey: "change-me"
+  }
+};
