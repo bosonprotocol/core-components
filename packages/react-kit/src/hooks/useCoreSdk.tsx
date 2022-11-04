6d4865dab2e9af2927f8738f1f380a551d23dfeb
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2022-11-04 17:48:47 +0000

    fix: do not recreate a web3 provider is not needed

--- packages/react-kit/src/hooks/useCoreSdk.tsx
+++ packages/react-kit/src/hooks/useCoreSdk.tsx
@@ -78,10 +78,9 @@ export function useCoreSdk(config: CoreSdkConfig) {
 
 function initCoreSdk(config: CoreSdkConfig) {
   const defaultConfig = getDefaultConfig(config.envName);
-  const defaultProvider = new providers.StaticJsonRpcProvider(
-    config.jsonRpcUrl || defaultConfig.jsonRpcUrl
-  );
-  const connectedProvider = config.web3Provider || defaultProvider;
+  const connectedProvider =
+    config.web3Provider ||
+    createDefaultProvider(config.jsonRpcUrl || defaultConfig.jsonRpcUrl);
   const metadataStorageUrl =
     config.ipfsMetadataStorageUrl || defaultConfig.ipfsMetadataUrl;
   const theGraphStorageUrl =
@@ -111,4 +110,36 @@ function initCoreSdk(config: CoreSdkConfig) {
     metaTx,
     lensContracts: config.lensContracts || defaultConfig.lens
   });
+}
+
+function createDefaultProvider(jsonRpcUrl: string): Provider {
+  const key = jsonRpcUrl.toLowerCase();
+  if (!providersCache.has(key)) {
+    const isInfura = isInfuraRpcNode(jsonRpcUrl);
+    console.log("create JsonRpcProvider", isInfura);
+    providersCache.set(
+      key,
+      isInfura.isInfura
+        ? new providers.InfuraProvider(isInfura.network, isInfura.apiKey)
+        : new providers.StaticJsonRpcProvider(jsonRpcUrl)
+    );
+  }
+  return providersCache.get(key) as Provider;
+}
+
+const providersCache = new Map<string, Provider>();
+
+function isInfuraRpcNode(jsonRpcUrl: string): {
+  isInfura: boolean;
+  network?: string;
+  apiKey?: string;
+} {
+  const regex = /^https:\/\/([\w-]+).infura.io\/[\w-]+\/([\w]+)$/;
+  const match = jsonRpcUrl.match(regex);
+  const isInfura: boolean = match !== null && match?.length === 3;
+  return {
+    isInfura,
+    network: isInfura ? (match as RegExpMatchArray)[1] : undefined,
+    apiKey: isInfura ? (match as RegExpMatchArray)[2] : undefined
+  };
 }
