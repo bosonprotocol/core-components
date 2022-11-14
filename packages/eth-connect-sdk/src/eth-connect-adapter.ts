57a91eeab94b400e1c9ede7c0028189c68fdb477
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2022-11-14 16:08:50 +0000

    chore: upgrade eth-connect-adapter to work in decentraland

--- packages/eth-connect-sdk/src/eth-connect-adapter.ts
+++ packages/eth-connect-sdk/src/eth-connect-adapter.ts
@@ -6,18 +6,37 @@ import {
   TransactionReceipt
 } from "@bosonprotocol/common";
 
+export type ExternalFeatures = {
+  delay: (ms: number) => Promise<undefined>;
+  getSignerAddress?: () => Promise<string>;
+};
+
+const TX_POLLING_DELAY = 2000;
+
 /**
  * `Web3LibAdapter` implementation targeting `eth-connect` in a Decentraland
  * environment.
  */
 export class EthConnectAdapter implements Web3LibAdapter {
   private _requestManager: RequestManager;
+  private _externalFeatures: ExternalFeatures;
 
-  constructor(requestManager: RequestManager) {
+  private static receiptData = new Map<string, { from: string; to: string }>();
+
+  constructor(
+    requestManager: RequestManager,
+    externalFeatures: ExternalFeatures
+  ) {
     this._requestManager = requestManager;
+    this._externalFeatures = externalFeatures;
   }
 
   public async getSignerAddress() {
+    if (this._externalFeatures?.getSignerAddress) {
+      const address = await this._externalFeatures?.getSignerAddress();
+      return address;
+    }
+    // Note: usage of requestManager.eth_accounts is prohibited by DCL kernel
     const [address] = await this._requestManager.eth_accounts();
     return address;
   }
@@ -41,19 +60,23 @@ export class EthConnectAdapter implements Web3LibAdapter {
   public async sendTransaction(
     transactionRequest: TransactionRequest
   ): Promise<TransactionResponse> {
+    console.log("sendTransaction()", transactionRequest);
+    const from = transactionRequest.from || (await this.getSignerAddress());
+    const to = transactionRequest.to || "0x";
     const txHash = await this._requestManager.eth_sendTransaction({
-      from: transactionRequest.from || "0x",
-      to: transactionRequest.to || "0x",
+      from,
+      to,
       data: transactionRequest.data || "0x",
       gas: transactionRequest.gasLimit?.toString(),
       gasPrice: transactionRequest.gasPrice?.toString(),
       value: transactionRequest.value?.toString(),
       nonce: transactionRequest.nonce?.toString()
     });
-
+    const receiptData = { from, to };
+    EthConnectAdapter.receiptData.set(txHash, receiptData);
     return {
       hash: txHash,
-      wait: async (confirmations?: number) => this._wait(txHash)
+      wait: async (confirmations?: number) => this._wait(txHash, receiptData)
     };
   }
 
@@ -72,14 +95,60 @@ export class EthConnectAdapter implements Web3LibAdapter {
     throw new Error("Not implemented");
   }
 
-  private async _wait(txHash: string, confirmations?: number): Promise<any> {
-    throw new Error("Not implemented");
+  private async _wait(
+    txHash: string,
+    receiptData: { from: string; to: string },
+    confirmations?: number
+  ): Promise<TransactionReceipt> {
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise<TransactionReceipt>(async (resolve, reject) => {
+      // Note: usage of requestManager.waitForCompletion is prohibited by DCL kernel (since 07-2021)
+      let receipt = null;
+      const account = await this.getSignerAddress();
+      const blockNumber = await this._requestManager.eth_blockNumber();
+      const presumedNonce = await this._requestManager.eth_getTransactionCount(
+        account,
+        blockNumber
+      );
+      let nonce = presumedNonce;
+      while (receipt == null && nonce <= presumedNonce) {
+        console.log("loop wait for receipt", txHash);
+        await this._externalFeatures.delay(TX_POLLING_DELAY);
+        receipt = await this._requestManager.eth_getTransactionReceipt(txHash);
+        const blockNumber = await this._requestManager.eth_blockNumber();
+        nonce = await this._requestManager.eth_getTransactionCount(
+          account,
+          blockNumber
+        );
+        console.log(
+          "tx presumed nonce",
+          presumedNonce,
+          "current on chain nonce",
+          nonce
+        );
+      }
+      console.log("receipt", receipt);
+      // try getting more data about the transaction
+
+      // receipt.status === 0 --> failed (TBC)
+      if (receipt && receipt.status === 0) {
+        reject(`Transaction failed:\n${receipt.transactionHash}`);
+      } else {
+        // Note: if nonce has increased, we can suppose the transaction has been speed up or cancelled
+        resolve(await this.getTransactionReceipt(txHash, receiptData));
+      }
+    });
   }
 
-  public async getTransactionReceipt(txHash): Promise<TransactionReceipt> {
-    const txObject = await this._requestManager.eth_getTransactionByHash(
-      txHash
-    );
+  public async getTransactionReceipt(
+    txHash,
+    receiptData?: { from: string; to: string }
+  ): Promise<TransactionReceipt> {
+    const txObject =
+      receiptData ||
+      EthConnectAdapter.receiptData.get(txHash) ||
+      // Note: usage of requestManager.eth_getTransactionByHash is prohibited by DCL kernel
+      (await this._requestManager.eth_getTransactionByHash(txHash));
 
     const txReceipt = await this._requestManager.eth_getTransactionReceipt(
       txHash
