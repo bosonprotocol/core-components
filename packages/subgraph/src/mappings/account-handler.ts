db29ab52281145d3fe5859760fb460185c0e528b
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2022-11-16 15:57:15 +0000

    add pendingSeller in subgraph

--- packages/subgraph/src/mappings/account-handler.ts
+++ packages/subgraph/src/mappings/account-handler.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Address, log, BigInt } from "@graphprotocol/graph-ts";
 import {
   SellerCreated,
   SellerUpdated,
+  SellerUpdatePending,
+  SellerUpdateApplied,
   BuyerCreated,
   DisputeResolverCreated,
   DisputeResolverActivated,
@@ -8,10 +12,12 @@ import {
   AllowedSellersAdded,
   AllowedSellersRemoved,
   DisputeResolverFeesAdded,
-  DisputeResolverFeesRemoved
+  DisputeResolverFeesRemoved,
+  DisputeResolverUpdateApplied,
+  DisputeResolverUpdatePending
 } from "../../generated/BosonAccountHandler/IBosonAccountHandler";
 import { IBosonVoucher } from "../../generated/BosonAccountHandler/IBosonVoucher";
-import { Seller, Buyer } from "../../generated/schema";
+import { Seller, Buyer, PendingSeller } from "../../generated/schema";
 
 import {
   getAndSaveDisputeResolver,
@@ -57,6 +63,7 @@ export function handleSellerCreatedEvent(event: SellerCreated): void {
   );
 }
 
+// Keep handleSellerUpdatedEvent for compatibility with v2.0.0
 export function handleSellerUpdatedEvent(event: SellerUpdated): void {
   const sellerFromEvent = event.params.seller;
   const authTokenFromEvent = event.params.authToken;
@@ -76,6 +83,79 @@ export function handleSellerUpdatedEvent(event: SellerUpdated): void {
   seller.authTokenType = authTokenFromEvent.tokenType;
   seller.active = sellerFromEvent.active;
   seller.save();
+
+  saveAccountEventLog(
+    event.transaction.hash.toHexString(),
+    event.logIndex,
+    "SELLER_UPDATED",
+    event.block.timestamp,
+    event.params.executedBy,
+    sellerId
+  );
+}
+
+export function handleSellerUpdatePendingEvent(
+  event: SellerUpdatePending
+): void {
+  const pendingSellerFromEvent = event.params.pendingSeller;
+  const pendingAuthTokenFromEvent = event.params.pendingAuthToken;
+  const sellerId = event.params.sellerId.toString();
+
+  let seller = Seller.load(sellerId);
+
+  if (!seller) {
+    seller = new Seller(sellerId);
+  }
+
+  let pendingSeller = PendingSeller.load(seller.id);
+  if (!pendingSeller) {
+    pendingSeller = new PendingSeller(seller.id);
+    pendingSeller.seller = seller.id;
+  }
+
+  pendingSeller.operator = pendingSellerFromEvent.operator;
+  pendingSeller.clerk = pendingSellerFromEvent.clerk;
+  pendingSeller.admin = pendingSellerFromEvent.admin;
+  pendingSeller.authTokenType = pendingAuthTokenFromEvent.tokenType;
+  pendingSeller.authTokenId = pendingAuthTokenFromEvent.tokenId;
+  pendingSeller.save();
+}
+
+export function handleSellerUpdateAppliedEvent(
+  event: SellerUpdateApplied
+): void {
+  const sellerFromEvent = event.params.seller;
+  const pendingSellerFromEvent = event.params.pendingSeller;
+  const authTokenFromEvent = event.params.authToken;
+  const pendingAuthTokenFromEvent = event.params.pendingAuthToken;
+  const sellerId = event.params.sellerId.toString();
+
+  let seller = Seller.load(sellerId);
+
+  if (!seller) {
+    seller = new Seller(sellerId);
+  }
+
+  seller.operator = sellerFromEvent.operator;
+  seller.admin = sellerFromEvent.admin;
+  seller.clerk = sellerFromEvent.clerk;
+  seller.treasury = sellerFromEvent.treasury;
+  seller.authTokenId = authTokenFromEvent.tokenId;
+  seller.authTokenType = authTokenFromEvent.tokenType;
+  seller.active = sellerFromEvent.active;
+  seller.save();
+  let pendingSeller = PendingSeller.load(seller.id);
+  if (!pendingSeller) {
+    pendingSeller = new PendingSeller(seller.id);
+    pendingSeller.seller = seller.id;
+  }
+
+  pendingSeller.operator = pendingSellerFromEvent.operator;
+  pendingSeller.clerk = pendingSellerFromEvent.clerk;
+  pendingSeller.admin = pendingSellerFromEvent.admin;
+  pendingSeller.authTokenType = pendingAuthTokenFromEvent.tokenType;
+  pendingSeller.authTokenId = pendingAuthTokenFromEvent.tokenId;
+  pendingSeller.save();
 
   saveAccountEventLog(
     event.transaction.hash.toHexString(),
@@ -129,10 +209,11 @@ export function handleDisputeResolverCreatedEvent(
   );
 }
 
+// Keep handleSellerUpdatedEvent for compatibility with v2.0.0
 export function handleDisputeResolverUpdatedEvent(
   event: DisputeResolverUpdated
 ): void {
-  const disputeResolverId = event.params.disputeResolver.id;
+  const disputeResolverId = event.params.disputeResolverId;
 
   getAndSaveDisputeResolver(disputeResolverId, event.address);
 
@@ -143,6 +224,26 @@ export function handleDisputeResolverUpdatedEvent(
     event.block.timestamp,
     event.params.executedBy,
     disputeResolverId.toString()
+  );
+}
+
+export function handleDisputeResolverUpdatePendingEvent(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  event: DisputeResolverUpdatePending
+): void {
+  log.error(
+    "handleDisputeResolverUpdatePendingEvent() Not implemented yet",
+    []
+  );
+}
+
+export function handleDisputeResolverUpdateAppliedEvent(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  event: DisputeResolverUpdateApplied
+): void {
+  log.error(
+    "handleDisputeResolverUpdateAppliedEvent() Not implemented yet",
+    []
   );
 }
 
