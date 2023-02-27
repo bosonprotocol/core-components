8f804e1029a96a22c368c598374d0cbb50f366ec
Albert Folch <albertfolch@redeemeum.com>
2023-02-27 15:32:10 +0100

    fix: erc mixins

--- packages/core-sdk/src/erc20/mixin.ts
+++ packages/core-sdk/src/erc20/mixin.ts
@@ -14,44 +14,44 @@ export class ERC20Mixin extends BaseCoreSDK {
   /*                           ERC20 related methods                          */
   /* -------------------------------------------------------------------------- */
   public async erc20Approve(
-    ...args: Omit<Parameters<typeof approve>, "web3Lib">
+    args: Omit<Parameters<typeof approve>[0], "web3Lib">
   ): Promise<ReturnType<typeof approve>> {
-    return approve({ web3Lib: this._web3Lib, ...args[0] });
+    return approve({ web3Lib: this._web3Lib, ...args });
   }
 
   public async erc20GetAllowance(
-    ...args: Omit<Parameters<typeof getAllowance>, "web3Lib">
+    args: Omit<Parameters<typeof getAllowance>[0], "web3Lib">
   ): Promise<ReturnType<typeof getAllowance>> {
-    return getAllowance({ web3Lib: this._web3Lib, ...args[0] });
+    return getAllowance({ web3Lib: this._web3Lib, ...args });
   }
 
   public async erc20GetDecimals(
-    ...args: Omit<Parameters<typeof getDecimals>, "web3Lib">
+    args: Omit<Parameters<typeof getDecimals>[0], "web3Lib">
   ): Promise<ReturnType<typeof getDecimals>> {
-    return getDecimals({ web3Lib: this._web3Lib, ...args[0] });
+    return getDecimals({ web3Lib: this._web3Lib, ...args });
   }
 
   public async erc20GetSymbol(
-    ...args: Omit<Parameters<typeof getSymbol>, "web3Lib">
+    args: Omit<Parameters<typeof getSymbol>[0], "web3Lib">
   ): Promise<ReturnType<typeof getSymbol>> {
-    return getSymbol({ web3Lib: this._web3Lib, ...args[0] });
+    return getSymbol({ web3Lib: this._web3Lib, ...args });
   }
 
   public async erc20GetName(
-    ...args: Omit<Parameters<typeof getName>, "web3Lib">
+    args: Omit<Parameters<typeof getName>[0], "web3Lib">
   ): Promise<ReturnType<typeof getName>> {
-    return getName({ web3Lib: this._web3Lib, ...args[0] });
+    return getName({ web3Lib: this._web3Lib, ...args });
   }
 
   public async erc20EnsureAllowance(
-    ...args: Omit<Parameters<typeof ensureAllowance>, "web3Lib">
+    args: Omit<Parameters<typeof ensureAllowance>[0], "web3Lib">
   ): Promise<ReturnType<typeof ensureAllowance>> {
-    return ensureAllowance({ web3Lib: this._web3Lib, ...args[0] });
+    return ensureAllowance({ web3Lib: this._web3Lib, ...args });
   }
 
   public async erc20BalanceOf(
-    ...args: Omit<Parameters<typeof balanceOf>, "web3Lib">
+    args: Omit<Parameters<typeof balanceOf>[0], "web3Lib">
   ): Promise<ReturnType<typeof balanceOf>> {
-    return balanceOf({ web3Lib: this._web3Lib, ...args[0] });
+    return balanceOf({ web3Lib: this._web3Lib, ...args });
   }
 }
