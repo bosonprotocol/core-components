23fcdd818d3191cff5f18566f5eab9abd65026de
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2023-02-27 15:23:33 +0100

    feat: omit web3lib erc mixins (#436)
    * feat: omit web3lib arg in erc mixins

--- packages/core-sdk/src/erc721/mixin.ts
+++ packages/core-sdk/src/erc721/mixin.ts
@@ -6,20 +6,20 @@ export class ERC721Mixin extends BaseCoreSDK {
   /*                           ERC721 related methods                          */
   /* -------------------------------------------------------------------------- */
   public async erc721BalanceOf(
-    ...args: Parameters<typeof balanceOf>
+    ...args: Omit<Parameters<typeof balanceOf>, "web3Lib">
   ): Promise<ReturnType<typeof balanceOf>> {
-    return balanceOf(...args);
+    return balanceOf({ web3Lib: this._web3Lib, ...args[0] });
   }
 
   public async erc721OwnerOf(
-    ...args: Parameters<typeof ownerOf>
+    ...args: Omit<Parameters<typeof ownerOf>, "web3Lib">
   ): Promise<ReturnType<typeof ownerOf>> {
-    return ownerOf(...args);
+    return ownerOf({ web3Lib: this._web3Lib, ...args[0] });
   }
 
   public async erc721TokenOfOwnerByIndex(
-    ...args: Parameters<typeof tokenOfOwnerByIndex>
+    ...args: Omit<Parameters<typeof tokenOfOwnerByIndex>, "web3Lib">
   ): Promise<ReturnType<typeof tokenOfOwnerByIndex>> {
-    return tokenOfOwnerByIndex(...args);
+    return tokenOfOwnerByIndex({ web3Lib: this._web3Lib, ...args[0] });
   }
 }
