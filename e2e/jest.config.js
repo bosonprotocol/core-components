d99f1a4cc1cf0447c7dd7819719a114a78256e8d
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-04-24 11:24:29 +0100

    chore: generate e2e tests coverage results

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,10 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const baseConfig = require("../jest.config.sdk");
+
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverage: false,
-  // moduleNameMapper: {
-  //   "^@bosonprotocol/(.*)$": "<rootDir>/../$1/"
-  // },
+  ...baseConfig,
+  rootDir: "..",
+  collectCoverageFrom: [
+    "<rootDir>/packages/**/*.{ts,js,jsx,tsx}",
+    "!<rootDir>/packages/react-kit/**",
+    "!<rootDir>/packages/subgraph/**"
+  ],
+  moduleNameMapper: {
+    "^@bosonprotocol/(.*)$": "<rootDir>/packages/$1/"
+  },
   globals: {
     "ts-jest": {
       tsconfig: "./tsconfig.tests.json"
