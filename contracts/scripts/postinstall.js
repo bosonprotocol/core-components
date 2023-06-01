d7db2160ceb5712986225ce8b4c2d25294f550a7
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-06-01 13:07:09 +0100

    chore: remove boson-protocol-contracts submodule (#506)
    * chore: remove contracts submodule
    
    * add npm dependencies to boson-protocol-contracts and seaport repos
    
    * simplify post install script
    
    * fix e2e test failure on CI pipeline
    
    * change CI to push all coverage report in the same flow
    
    * fix typo in CI pipeline
    
    * ci pipeline : disable cache when re-executing the package tests

---
+++ contracts/scripts/postinstall.js
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { symlinkSync, existsSync, rmSync, mkdirSync } = require("fs");
+const { resolve } = require("path");
+
+const createLink = async (linkPath, target) => {
+  while (existsSync(linkPath)) {
+    // remove linkPath first
+    rmSync(linkPath, { recursive: true });
+    await new Promise((r) => setTimeout(r, 200));
+  }
+  const parentDir = resolve(linkPath, "..");
+  if (!existsSync(parentDir)) {
+    console.log(`Create parent dir ${parentDir}`);
+    mkdirSync(parentDir);
+  }
+  console.log(`Create link ${linkPath} --> ${target}`);
+  symlinkSync(target, linkPath, "junction");
+};
+
+const protocol = {
+  target: `${resolve(
+    __dirname,
+    "..",
+    "node_modules",
+    "@bosonprotocol/boson-protocol-contracts"
+  )}`,
+  linkPath: `${resolve(__dirname, "..", "protocol-contracts")}`
+};
+
+const seaport = {
+  target: `${resolve(__dirname, "..", "node_modules", "seaport")}`,
+  linkPath: `${resolve(
+    __dirname,
+    "..",
+    "protocol-contracts/submodules/seaport"
+  )}`
+};
+
+async function main() {
+  for (const entry of [protocol, seaport]) {
+    await createLink(entry.linkPath, entry.target);
+  }
+}
+
+main()
+  .then(() => console.log("success"))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
