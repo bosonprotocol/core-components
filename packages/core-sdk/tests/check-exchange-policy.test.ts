e2d369d20655720f04ce52938b39092ca6a016be
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-07-07 16:38:48 +0100

    feat: implement checkExchangePolicy service at core-sdk level

---
+++ packages/core-sdk/tests/check-exchange-policy.test.ts
@@ -0,0 +1,115 @@
+import { offers } from "../src";
+import invalidOffer1 from "./exchangePolicy/examples/invalidOffer1.json";
+import invalidOffer2 from "./exchangePolicy/examples/invalidOffer2.json";
+import invalidOffer3 from "./exchangePolicy/examples/invalidOffer3.json";
+import invalidOffer4 from "./exchangePolicy/examples/invalidOffer4.json";
+import invalidOffer5 from "./exchangePolicy/examples/invalidOffer5.json";
+import validOffer from "./exchangePolicy/examples/validOffer.json";
+import exchangePolicyRules from "./exchangePolicy/exchangePolicyRules.testing.json";
+import { OfferFieldsFragment } from "../src/subgraph";
+
+describe("test check exchange policy", () => {
+  test("test check exchange policy with validOffer data", () => {
+    const result = offers.checkExchangePolicy(
+      validOffer as OfferFieldsFragment,
+      exchangePolicyRules
+    );
+    expect(result.isValid).toBe(true);
+  });
+  test("test check exchange policy with invalidOffer data (1 error)", () => {
+    const result = offers.checkExchangePolicy(
+      invalidOffer1 as OfferFieldsFragment,
+      exchangePolicyRules
+    );
+    expect(result.isValid).toBe(false);
+    expect(result.errors.length).toEqual(1);
+    expect(result.errors[0].message).toEqual(
+      "Dispute Period Duration is less than 30 days"
+    );
+  });
+  test("test check exchange policy with invalidOffer data (all errors)", () => {
+    const result = offers.checkExchangePolicy(
+      invalidOffer2 as OfferFieldsFragment,
+      exchangePolicyRules
+    );
+    expect(result.isValid).toBe(false);
+    const messagePerPath = {
+      disputePeriodDuration: "Dispute Period Duration is less than 30 days",
+      disputeResolverId: "Dispute Resolver is not whitelisted",
+      "exchangeToken.address": "Currency Token is not whitelisted",
+      resolutionPeriodDuration:
+        "Resolution Period Duration is less than 15 days",
+      "metadata.type": "Metadata Type is not PRODUCT_V1 standard",
+      "metadata.exchangePolicy.template":
+        "Buyer/Seller Agreement Template is not standard",
+      "metadata.shipping.returnPeriodInDays":
+        "Return Period is less than 15 days"
+    };
+    expect(result.errors.length).toEqual(Object.keys(messagePerPath).length);
+    for (const path in messagePerPath) {
+      expect(result.errors.find((error) => error.path === path)).toBeTruthy();
+      expect(
+        result.errors.find((error) => error.path === path)?.message
+      ).toEqual(messagePerPath[path]);
+    }
+  });
+  test("test check exchange policy with invalidOffer data (missing data 1/3)", () => {
+    const result = offers.checkExchangePolicy(
+      invalidOffer3 as unknown as OfferFieldsFragment,
+      exchangePolicyRules
+    );
+    expect(result.isValid).toBe(false);
+    const messagePerPath = {
+      "metadata.type": "Metadata Type is not specified",
+      "metadata.exchangePolicy.template":
+        "Buyer/Seller Agreement Template is not specified",
+      "metadata.shipping.returnPeriodInDays": "Return Period is not specified"
+    };
+    expect(result.errors.length).toEqual(Object.keys(messagePerPath).length);
+    for (const path in messagePerPath) {
+      expect(result.errors.find((error) => error.path === path)).toBeTruthy();
+      expect(
+        result.errors.find((error) => error.path === path)?.message
+      ).toEqual(messagePerPath[path]);
+    }
+  });
+  test("test check exchange policy with invalidOffer data (missing data 2/3)", () => {
+    const result = offers.checkExchangePolicy(
+      invalidOffer4 as unknown as OfferFieldsFragment,
+      exchangePolicyRules
+    );
+    expect(result.isValid).toBe(false);
+    const messagePerPath = {
+      "metadata.type": "Metadata Type is not specified",
+      "metadata.exchangePolicy.template":
+        "Buyer/Seller Agreement Template is not specified",
+      "metadata.shipping.returnPeriodInDays": "Return Period is not specified"
+    };
+    expect(result.errors.length).toEqual(Object.keys(messagePerPath).length);
+    for (const path in messagePerPath) {
+      expect(result.errors.find((error) => error.path === path)).toBeTruthy();
+      expect(
+        result.errors.find((error) => error.path === path)?.message
+      ).toEqual(messagePerPath[path]);
+    }
+  });
+  test("test check exchange policy with invalidOffer data (missing data 3/3)", () => {
+    const result = offers.checkExchangePolicy(
+      invalidOffer5 as unknown as OfferFieldsFragment,
+      exchangePolicyRules
+    );
+    expect(result.isValid).toBe(false);
+    const messagePerPath = {
+      "metadata.exchangePolicy.template":
+        "Buyer/Seller Agreement Template is not specified",
+      "metadata.shipping.returnPeriodInDays": "Return Period is not specified"
+    };
+    expect(result.errors.length).toEqual(Object.keys(messagePerPath).length);
+    for (const path in messagePerPath) {
+      expect(result.errors.find((error) => error.path === path)).toBeTruthy();
+      expect(
+        result.errors.find((error) => error.path === path)?.message
+      ).toEqual(messagePerPath[path]);
+    }
+  });
+});
--- scripts/checkOfferExchangePolicy.ts
+++ scripts/checkOfferExchangePolicy.ts
@@ -1,45 +1,31 @@
-import fs from "fs";
-import { buildYup } from "schema-to-yup";
-import { SchemaOf } from "yup";
-
 import { EnvironmentType } from "@bosonprotocol/common/src/types/configs";
 import { providers } from "ethers";
 import { program } from "commander";
 import { getDefaultConfig } from "@bosonprotocol/common/src";
 import { CoreSDK } from "../packages/core-sdk/src";
 import { EthersAdapter } from "../packages/ethers-sdk/src";
+import rulesTesting from "../packages/core-sdk/tests/exchangePolicy/exchangePolicyRules.testing.json";
+import rulesStaging from "../packages/core-sdk/tests/exchangePolicy/exchangePolicyRules.staging.json";
+import rulesProduction from "../packages/core-sdk/tests/exchangePolicy/exchangePolicyRules.production.json";
+
+const rules = {
+  testing: rulesTesting,
+  staging: rulesStaging,
+  production: rulesProduction
+};
 
 program
   .description("Check Exchange policy of an Offer.")
+  .argument("<OFFER_ID>", "Id of the offer to check")
   .option("-e, --env <ENV_NAME>", "Target environment", "testing")
   .parse(process.argv);
 
-const schema = {
-  $schema: "http://json-schema.org/draft-07/schema#",
-  $id: "http://example.com/person.schema.json",
-  title: "Person",
-  description: "A person",
-  type: "object",
-  properties: {
-    disputePeriodDuration: {
-      description: "disputePeriodDuration",
-      type: "number",
-      max: 3600 * 24 * 30 // 30 days to sec
-    }
-  }
-};
-
-const baseSchema: SchemaOf<any> = buildYup(schema, {});
-
 async function main() {
-  //   const [sellerPrivateKey] = program.args;
-  const offerId = "10";
+  const [offerId] = program.args;
 
   const opts = program.opts();
   const envName = opts.env || "testing";
   const defaultConfig = getDefaultConfig(envName as EnvironmentType);
-  const chainId = defaultConfig.chainId;
-//   const sellerWallet = new Wallet(sellerPrivateKey);
 
   const coreSDK = CoreSDK.fromDefaultConfig({
     web3Lib: new EthersAdapter(
@@ -48,11 +34,8 @@ async function main() {
     envName
   });
 
-  const offerData = await coreSDK.getOfferById(offerId);
-  console.log("offerData", offerData);
-
-  const result = baseSchema.validateSync(offerData);
-  console.log("result", result);
+  const result = await coreSDK.checkExchangePolicy(offerId, rules[envName]);
+  console.log(result);
 }
 
 main()
