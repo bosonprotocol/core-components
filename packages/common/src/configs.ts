425c63bb4c8ac704be0a6bc2cc90d24ce8249a28
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-08-24 16:12:39 +0100

    update local contracts addresses

--- packages/common/src/configs.ts
+++ packages/common/src/configs.ts
@@ -96,11 +96,11 @@ export const envConfigs: Record<EnvironmentType, ProtocolConfig[]> = {
       ipfsMetadataUrl: "http://127.0.0.1:5001",
       contracts: {
         protocolDiamond: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
-        testErc20: "0x998abeb3E57409262aE5b751f60747921B33613E", // Foreign20 contract
-        testErc721: "0x70e0bA845a1A0F2DA3359C97E0285013525FFC49", // Foreign721 contract
-        testErc1155: "0x4826533B4897376654Bb4d4AD88B7faFD0C98528", // Foreign1155 contract
+        testErc20: "0x95401dc811bb5740090279Ba06cfA8fcF6113778", // Foreign20 contract
+        testErc721: "0x998abeb3E57409262aE5b751f60747921B33613E", // Foreign721 contract
+        testErc1155: "0x70e0bA845a1A0F2DA3359C97E0285013525FFC49", // Foreign1155 contract
         forwarder: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0", // MockForwarder contract
-        seaport: "0x0E801D84Fa97b50751Dbf25036d067dCf18858bF" // MockSeaport contract
+        seaport: "0x99bbA657f2BbC93c02D617f8bA121cB8Fc104Acf" // MockSeaport contract
       },
       metaTx: {
         relayerUrl: "http://localhost:8888",
