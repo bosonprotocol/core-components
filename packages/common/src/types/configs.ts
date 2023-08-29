89aa6861bdbdbec23ec274a38c3b02ef6734fa3f
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2023-08-29 16:42:58 +0200

    feat: add default tokens to protocol config (#548)
    * feat: add default tokens to protocol config
    
    * refactor: default tokens and other mappings

--- packages/common/src/types/configs.ts
+++ packages/common/src/types/configs.ts
@@ -31,13 +31,21 @@ export type Lens = LensContracts & {
   ipfsGateway?: string | undefined;
 };
 
-type ChainId = typeof chains[keyof typeof chains];
+export type ChainId = typeof chains[keyof typeof chains];
 
 export type ConfigId = `${EnvironmentType}-${ChainId}-${number}`;
+
+export type Token = {
+  symbol: string;
+  name: string;
+  address: string;
+  decimals: string;
+};
 
 export type ProtocolConfig = {
   envName: EnvironmentType;
   configId: ConfigId;
+  defaultTokens: Token[];
   defaultDisputeResolverId: string;
   sellersBlackList: string;
   chainId: ChainId;
