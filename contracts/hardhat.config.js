0af20df916cc3c603f9c1cdcfd70ca1d67f6f3dc
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-09-25 13:25:11 +0100

    fix: force amd64 platform on dockers (#569)
    * fix: force amd64 platform on dockers
    
    * refresh image tags for dockers, to force them to rebuild
    
    * chore: add documentation for seller account creation

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -88,7 +88,16 @@ module.exports = {
         }
       },
       {
-        version: "0.8.17"
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+            details: {
+              yul: true
+            }
+          }
+        }
       },
       {
         version: "0.4.17"
