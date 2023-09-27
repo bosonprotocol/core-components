30cb56d980c169cb4c01952670f7d1863fec67b0
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-09-27 18:20:50 +0100

    fix: ensure identification of ProductV1ProductOverrides is correct (#574)

--- packages/subgraph/src/entities/metadata/product-v1/product.ts
+++ packages/subgraph/src/entities/metadata/product-v1/product.ts
@@ -24,10 +24,10 @@ export function getProductId(uuid: string, version: string): string {
 }
 
 export function getProductOverridesId(
-  productId: string,
-  overridesVersion: string
+  sellerId: string,
+  offerId: string
 ): string {
-  return `${productId}-overrides-${overridesVersion}`;
+  return `${sellerId}-overrides-${offerId}`;
 }
 
 export function getCategoryId(categoryName: string): string {
@@ -198,7 +198,6 @@ export function saveProductV1ProductOrOverrides(
   }
 
   return saveProductV1ProductOverrides(
-    getProductId(uuid, version.toString()),
     version,
     title,
     description,
@@ -219,7 +218,8 @@ export function saveProductV1ProductOrOverrides(
     packaging_dimensions_height,
     packaging_dimensions_unit,
     packaging_weight_value,
-    packaging_weight_unit
+    packaging_weight_unit,
+    offer
   );
 }
 
@@ -409,7 +409,6 @@ function getMax(num1: BigInt, num2: BigInt): BigInt {
 }
 
 function saveProductV1ProductOverrides(
-  productId: string,
   version: i32,
   title: string,
   description: string,
@@ -430,16 +429,17 @@ function saveProductV1ProductOverrides(
   packaging_dimensions_height: string,
   packaging_dimensions_unit: string,
   packaging_weight_value: string,
-  packaging_weight_unit: string
+  packaging_weight_unit: string,
+  offer: Offer
 ): string {
   const productOverridesId = getProductOverridesId(
-    productId,
-    version.toString()
+    offer.sellerId.toString(),
+    offer.id
   );
-  let productOverrides = ProductV1ProductOverrides.load(productId);
+  let productOverrides = ProductV1ProductOverrides.load(productOverridesId);
 
   if (!productOverrides) {
-    productOverrides = new ProductV1ProductOverrides(productId);
+    productOverrides = new ProductV1ProductOverrides(productOverridesId);
   }
 
   productOverrides.version = version;
@@ -618,7 +618,9 @@ function removeSalesChannel(
     const length = product.salesChannels ? salesChannels.length : 0;
     const newSalesChannels: string[] = [];
     for (let i = 0; i < length; i++) {
-      const oldSalesChannelId = salesChannels ? (salesChannels[i] as string) : "";
+      const oldSalesChannelId = salesChannels
+        ? (salesChannels[i] as string)
+        : "";
       if (oldSalesChannelId != salesChannelId) {
         newSalesChannels.push(oldSalesChannelId);
       }
