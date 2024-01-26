4c095f821e826ced9c8747799c0034180a77e5d4
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2024-01-26 18:17:42 +0100

    fix: colors custom store (#655)

--- packages/react-kit/src/components/styles/ResetStylesForNonWidgets.tsx
+++ packages/react-kit/src/components/styles/ResetStylesForNonWidgets.tsx
@@ -5,4 +5,19 @@ const colors = theme.colors.light;
 export const ResetStylesForNonWidgets = styled.div`
   // TODO: check if we can avoid this by using shadow dom (react-shadow for example)
   color: ${colors.black};
+  --l: 50%;
+  --headerBgColor: ${colors.white};
+  --headerTextColor: ${colors.darkGrey};
+  --primary: ${colors.primary};
+  --secondary: ${colors.lightGrey};
+  --accent: ${colors.accent};
+  --accentNoDefault: initial;
+  --accentDark: ${colors.arsenic};
+  --textColor: ${colors.black};
+  --primaryBgColor: ${colors.primaryBgColor};
+  --secondaryBgColor: ${colors.secondary};
+  --footerBgColor: ${colors.black};
+  --footerTextColor: ${colors.white};
+  --buttonBgColor: ${colors.primary};
+  --buttonTextColor: ${colors.black};
 `;
