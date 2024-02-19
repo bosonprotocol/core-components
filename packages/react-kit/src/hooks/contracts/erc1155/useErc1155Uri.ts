171225ce1d18dc1f10b215ecdfff48a5691293c9
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2024-02-19 09:29:24 +0100

    Update packages/react-kit/src/hooks/contracts/erc1155/useErc1155Uri.ts
    Co-authored-by: Ludovic Levalleux <levalleux_ludo@hotmail.com>

--- packages/react-kit/src/hooks/contracts/erc1155/useErc1155Uri.ts
+++ packages/react-kit/src/hooks/contracts/erc1155/useErc1155Uri.ts
@@ -9,20 +9,24 @@ export const useErc1155Uri = (
   { enabled }: { enabled: boolean | undefined }
 ) => {
   const coreSDK = useCoreSDKWithContext();
-  return useQuery(["useErc1155Uri", coreSDK.uuid, props], async () => {
-    const { contractAddress, tokenIds } = props;
-    if (!contractAddress || !tokenIds) {
-      return;
-    }
-    return Promise.all(
-      tokenIds.map((tokenId) =>
-        tokenId
-          ? coreSDK.erc1155Uri({
-              contractAddress,
-              tokenId
-            })
-          : null
-      )
-    );
-  });
+  return useQuery(
+    ["useErc1155Uri", coreSDK.uuid, props],
+    async () => {
+      const { contractAddress, tokenIds } = props;
+      if (!contractAddress || !tokenIds) {
+        return;
+      }
+      return Promise.all(
+        tokenIds.map((tokenId) =>
+          tokenId
+            ? coreSDK.erc1155Uri({
+                contractAddress,
+                tokenId
+              })
+            : null
+        )
+      );
+    },
+    { enabled }
+  );
 };
