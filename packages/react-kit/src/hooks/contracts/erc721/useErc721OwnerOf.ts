22ab128b2d3776a30283b573526f3405dae41627
Albert Folch <albertfolch@redeemeum.com>
2024-02-19 09:31:15 +0100

    feat: add enabled to use erc721 owner of

--- packages/react-kit/src/hooks/contracts/erc721/useErc721OwnerOf.ts
+++ packages/react-kit/src/hooks/contracts/erc721/useErc721OwnerOf.ts
@@ -9,20 +9,26 @@ export const useErc721OwnerOf = (
   { enabled }: { enabled: boolean | undefined }
 ) => {
   const coreSDK = useCoreSDKWithContext();
-  return useQuery(["erc721-owner-of", coreSDK.uuid, props], async () => {
-    const { contractAddress, tokenIds } = props;
-    if (!contractAddress || !tokenIds) {
-      return;
+  return useQuery(
+    ["erc721-owner-of", coreSDK.uuid, props],
+    async () => {
+      const { contractAddress, tokenIds } = props;
+      if (!contractAddress || !tokenIds) {
+        return;
+      }
+      return Promise.all(
+        tokenIds.map((tokenId) =>
+          tokenId
+            ? coreSDK.erc721OwnerOf({
+                contractAddress,
+                tokenId
+              })
+            : null
+        )
+      );
+    },
+    {
+      enabled
     }
-    return Promise.all(
-      tokenIds.map((tokenId) =>
-        tokenId
-          ? coreSDK.erc721OwnerOf({
-              contractAddress,
-              tokenId
-            })
-          : null
-      )
-    );
-  });
+  );
 };
