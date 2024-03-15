95f47cd29c7a27935922fd9711a6df73da5255d8
Albert Folch <albertfolch@redeemeum.com>
2024-03-15 10:48:56 +0100

    attempt fix chromatic env vars

--- packages/react-kit/src/lib/config/config.ts
+++ packages/react-kit/src/lib/config/config.ts
@@ -1,12 +1,12 @@
 import { getRpcUrls } from "../const/networks";
 
-const magicLinkKey = process.env.REACT_APP_MAGIC_API_KEY;
+const magicLinkKey = process.env.STORYBOOK_REACT_APP_MAGIC_API_KEY;
 if (!magicLinkKey) {
-  throw new Error("REACT_APP_MAGIC_API_KEY is not defined");
+  throw new Error("STORYBOOK_REACT_APP_MAGIC_API_KEY is not defined");
 }
-const infuraKey = process.env.INFURA_PROJECT_SECRET;
+const infuraKey = process.env.STORYBOOK_INFURA_PROJECT_SECRET;
 if (!infuraKey) {
-  throw new Error("INFURA_PROJECT_SECRET is not defined");
+  throw new Error("STORYBOOK_INFURA_PROJECT_SECRET is not defined");
 }
 
 export const CONFIG = {
