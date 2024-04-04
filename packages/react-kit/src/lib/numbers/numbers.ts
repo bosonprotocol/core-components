25a73622b3d21699f84e3878814312092ea9cc47
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2024-04-04 14:18:19 +0100

    fix return type for getNumberWithDecimals

--- packages/react-kit/src/lib/numbers/numbers.ts
+++ packages/react-kit/src/lib/numbers/numbers.ts
@@ -11,7 +11,7 @@ export const getNumberWithDecimals = (
   value: string,
   decimals: string
 ): number => {
-  return utils.formatUnits(value, decimals);
+  return Number(utils.formatUnits(value, decimals));
 };
 
 /**
