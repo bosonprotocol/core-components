df84d2ef89c4d371d46cdb9eb446296079a31b95
Albert Folch <albertfolch@redeemeum.com>
2024-05-27 17:31:18 +0200

    attempt 2 chromatic

--- packages/react-kit/src/lib/env/getEnvVar.ts
+++ packages/react-kit/src/lib/env/getEnvVar.ts
@@ -1,4 +1,3 @@
 export const getEnvVar = (key: string): string | undefined => {
-  // @ts-expect-error import.meta.env only exists in vite environments
-  return import.meta?.env?.[key] ?? process?.env?.[key];
+  return /*import.meta?.env?.[key] ??*/ process?.env?.[key];
 };
