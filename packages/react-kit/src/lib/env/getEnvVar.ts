3636bc4fd66c160938dd4ac43d6ba2071e6cc290
Albert Folch <albertfolch@redeemeum.com>
2024-05-27 17:52:40 +0200

    attempt 5

--- packages/react-kit/src/lib/env/getEnvVar.ts
+++ packages/react-kit/src/lib/env/getEnvVar.ts
@@ -1,4 +1,4 @@
 export const getEnvVar = (key: string): string | undefined => {
   // @ts-expect-error import.meta.env only exists in vite environments
-  return import.meta?.env?.[key] || process?.env?.[key];
+  return process?.env?.[key] || import.meta?.env?.[key];
 };
