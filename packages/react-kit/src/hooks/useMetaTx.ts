e07b39ef87d1c396b74b90a70b9a5d84f5c7bb30
Albert Folch <albertfolch@redeemeum.com>
2024-09-16 12:37:44 +0200

    chore: export cta related stuff

--- packages/react-kit/src/hooks/useMetaTx.ts
+++ packages/react-kit/src/hooks/useMetaTx.ts
@@ -1,7 +1,9 @@
 import { CoreSDK } from "@bosonprotocol/core-sdk";
 import { useSignerAddress } from "./useSignerAddress";
 
-export function useMetaTx(coreSdk: CoreSDK | undefined) {
+export function useMetaTx(
+  coreSdk: Pick<CoreSDK, "isMetaTxConfigSet" | "web3Lib"> | undefined
+) {
   const { signerAddress, signerContract } = useSignerAddress(coreSdk?.web3Lib);
   const isMetaTx = coreSdk
     ? Boolean(coreSdk.isMetaTxConfigSet && signerAddress && !signerContract)
