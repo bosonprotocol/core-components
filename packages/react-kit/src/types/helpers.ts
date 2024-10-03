dba0c0094b8d44964dbe35670366e7e57ea1d823
Albert Folch <albertfolch@redeemeum.com>
2024-10-03 12:44:21 +0200

    remove unused lint rule

--- packages/react-kit/src/types/helpers.ts
+++ packages/react-kit/src/types/helpers.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 export type DeepReadonly<T> = {
   // TODO: remove once https://github.com/microsoft/TypeScript/issues/13923 is merged
