b88b250c0eb7b2b43f85ab58f38055868f06855f
Albert Folch <albertfolch@redeemeum.com>
2025-01-21 12:13:03 +0100

    feat: add border width prop to loading

--- packages/react-kit/src/components/ui/loading/Loading.tsx
+++ packages/react-kit/src/components/ui/loading/Loading.tsx
@@ -5,15 +5,23 @@ export const Loading = ({
   color,
   size = 1.5625, // in rem = 25px
   theme,
-  style
+  style,
+  borderWidthPx = 5
 }: {
   color?: CSSProperties["color"];
   size?: number;
   theme?: { colors?: { light?: { accent: CSSProperties["color"] } } };
   style?: CSSProperties;
+  borderWidthPx?: number;
 }) => {
   return (
-    <LoadingStyle $color={color} $size={size} theme={theme} style={style} />
+    <LoadingStyle
+      $color={color}
+      $size={size}
+      theme={theme}
+      style={style}
+      $borderWidthPx={borderWidthPx}
+    />
   );
 };
 
@@ -25,12 +33,13 @@ const loadingAnimation = keyframes`
 const LoadingStyle = styled.span.attrs({ className: "loading" })<{
   $color?: CSSProperties["color"];
   $size?: number;
+  $borderWidthPx: number;
 }>`
   height: ${({ $size }) => $size}rem;
   width: ${({ $size }) => $size}rem;
   min-height: ${({ $size }) => $size}rem;
   min-width: ${({ $size }) => $size}rem;
-  border: 5px solid
+  border: ${({ $borderWidthPx }) => $borderWidthPx}px solid
     ${({ theme, $color }) =>
       theme?.colors?.light?.accent || $color || "#000000"};
   border-bottom-color: transparent;
