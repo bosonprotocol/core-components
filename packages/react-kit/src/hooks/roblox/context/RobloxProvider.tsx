0b39b1a0da50dac88bb700a3acdd312968cfd339
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2025-01-29 17:56:10 +0100

    fix: process is not defined error (#895)
    * fix: process is not defined error

--- packages/react-kit/src/hooks/roblox/context/RobloxProvider.tsx
+++ packages/react-kit/src/hooks/roblox/context/RobloxProvider.tsx
@@ -1,20 +1,36 @@
+import { ConfigId, EnvironmentType } from "@bosonprotocol/core-sdk";
 import { useEnvContext } from "../../../components/environment/EnvironmentContext";
 import { RobloxContext } from "./RobloxContext";
 import { envConfigs } from "@bosonprotocol/roblox-sdk";
-import React from "react";
+import React, { useMemo } from "react";
+
+const getBackendOrigin = ({
+  envName,
+  configId
+}: {
+  envName: EnvironmentType;
+  configId: ConfigId;
+}) => {
+  return (
+    // @ts-expect-error import.meta.env only exists in vite environments
+    import.meta.env?.STORYBOOK_ROBLOX_BACKEND_ORIGIN ||
+    (typeof process !== "undefined" &&
+      process.env?.STORYBOOK_ROBLOX_BACKEND_ORIGIN) ||
+    envConfigs[envName].find((conf) => conf.configId === configId)
+      ?.backendOrigin ||
+    ""
+  );
+};
 
 export type RobloxProviderProps = {
   children: React.ReactNode;
 };
 export const RobloxProvider: React.FC<RobloxProviderProps> = ({ children }) => {
   const { configId, envName } = useEnvContext();
-  const backendOrigin =
-    // @ts-expect-error import.meta.env only exists in vite environments
-    import.meta?.env?.STORYBOOK_ROBLOX_BACKEND_ORIGIN ||
-    process?.env?.STORYBOOK_ROBLOX_BACKEND_ORIGIN ||
-    envConfigs[envName].find((conf) => conf.configId === configId)
-      ?.backendOrigin ||
-    "";
+  const backendOrigin = useMemo(
+    () => getBackendOrigin({ envName, configId }),
+    [envName, configId]
+  );
   return (
     <RobloxContext.Provider value={{ backendOrigin }}>
       {children}
