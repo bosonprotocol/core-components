4c011cc491c81ecca50f16567f98dbfc30a0d81c
Albert Folch <albertfolch@redeemeum.com>
2025-03-21 15:23:52 +0100

    feat: make uploaded single pdf clickable

--- packages/react-kit/src/components/form/Upload/UploadedSinglePdfFile.tsx
+++ packages/react-kit/src/components/form/Upload/UploadedSinglePdfFile.tsx
@@ -3,17 +3,52 @@ import { FilePdf, X } from "phosphor-react";
 import { Typography } from "../../ui/Typography";
 import React from "react";
 import { colors } from "../../../colors";
-
+import { getIpfsGatewayUrl } from "../../../lib/ipfs/ipfs";
+import styled, { css } from "styled-components";
+const Wrapper = styled(Grid)<
+  Pick<UploadedSinglePdfFileProps, "onClick" | "urlProps">
+>`
+  ${({ onClick, urlProps }) =>
+    onClick || urlProps
+      ? css`
+          cursor: pointer;
+        `
+      : ""}
+`;
 export type UploadedSinglePdfFileProps = {
   fileName: string;
   onXClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  className?: string;
+  onClick?: () => unknown;
+  urlProps?: {
+    uri: Parameters<typeof getIpfsGatewayUrl>[0];
+    opts: Parameters<typeof getIpfsGatewayUrl>[1];
+    linkProps?: Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "onClick">;
+  };
 };
 export const UploadedSinglePdfFile = ({
   fileName,
-  onXClick
+  onClick,
+  onXClick,
+  className,
+  urlProps
 }: UploadedSinglePdfFileProps) => {
   return (
-    <Grid flexDirection="row" alignItems="center" gap="0.25rem">
+    <Wrapper
+      {...(urlProps
+        ? {
+            target: "_blank",
+            rel: "noopener noreferrer",
+            ...urlProps.linkProps,
+            href: getIpfsGatewayUrl(urlProps.uri, urlProps.opts),
+            tag: "a"
+          }
+        : { onClick })}
+      flexDirection="row"
+      alignItems="center"
+      gap="0.25rem"
+      className={className}
+    >
       <div>
         <FilePdf size={24} />
       </div>
@@ -31,11 +66,14 @@ export const UploadedSinglePdfFile = ({
             display: "flex",
             justifyContent: "center"
           }}
-          onClick={onXClick}
+          onClick={(e) => {
+            e.preventDefault();
+            onXClick(e);
+          }}
         >
           <X size={12.5} />
         </button>
       )}
-    </Grid>
+    </Wrapper>
   );
 };
