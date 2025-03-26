2c951ee32fc25c918948cb1a63735856e0930bfb
Albert Folch <albertfolch@redeemeum.com>
2025-03-26 11:50:48 +0100

    wip

--- packages/react-kit/src/components/ui/Typography.tsx
+++ packages/react-kit/src/components/ui/Typography.tsx
@@ -89,10 +89,8 @@ const Wrapper = styled.div<InnerTypographyProps>`
 
 export type TypographyProps = WrapperProps &
   HTMLAttributes<unknown> & {
-    children?: string | React.ReactNode;
+    children?: React.ReactNode;
     tag?: keyof JSX.IntrinsicElements;
-    style?: React.CSSProperties;
-    className?: string;
   };
 
 export const Typography = forwardRef<HTMLDivElement, TypographyProps>(
