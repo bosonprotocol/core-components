ec51a36cc1226bf71308ff2a6560e15e49a38dd2
Albert Folch <albertfolch@redeemeum.com>
2025-03-26 15:37:03 +0100

    feat: exports erc20 hooks

--- packages/react-kit/src/hooks/index.ts
+++ packages/react-kit/src/hooks/index.ts
@@ -1,38 +1,40 @@
-export { useCoreSdk } from "./core-sdk/useCoreSdk";
-export { useIpfsMetadataStorage } from "./useIpfsMetadataStorage";
-export { useIpfsContext } from "../components/ipfs/IpfsContext";
-export * from "./magic";
-export * from "../lib/signer/externalSigner";
-export * from "../lib/signer/useCallSignerFromIframe";
-export * from "./useEffectDebugger";
-export * from "./useHandleText";
-export * from "./useExchanges";
-export * from "./useMetaTx";
-export * from "../components/widgets/finance/useOffersBacked";
-export * from "./contracts/useGetTokenUriImages";
-export * from "./products/useProductByUuid";
-export * from "./bundles/useBundleByUuid";
-export * from "./contracts/erc721/useErc721TokenUris";
-export * from "./contracts/erc721/useErc721Name";
-export * from "./contracts/erc721/useErc721OwnerOf";
-export * from "./contracts/erc1155/useErc1155Name";
-export * from "./contracts/erc1155/useErc1155Uris";
-export * from "./uniswap/useIsWindowVisible";
-export * from "./connection/connection";
 export { useConfigContext } from "../components/config/ConfigContext";
-export * from "./storage/useLocalStorage";
-export * from "./ipfs/useIpfsStorage";
-export * from "./location/buildUseSearchParams";
-export * from "./form/useForm";
-export * from "./useBreakpoints";
-export * from "./useSignerAddress";
-export * from "./useMetaTx";
-export { useCtaClickHandler } from "./useCtaClickHandler";
+export { useIpfsContext } from "../components/ipfs/IpfsContext";
 export {
-  useToggleAccountDrawer,
   useAccountDrawer,
   useCloseAccountDrawer,
-  useOpenAccountDrawer
+  useOpenAccountDrawer,
+  useToggleAccountDrawer
 } from "../components/wallet2/accountDrawer/index";
-export * from "./usePrevious";
+export * from "../components/widgets/finance/useOffersBacked";
+export * from "../lib/signer/externalSigner";
+export * from "../lib/signer/useCallSignerFromIframe";
+export * from "./bundles/useBundleByUuid";
+export * from "./connection/connection";
+export * from "./contracts/erc20/useErc20Allowance";
+export * from "./contracts/erc20/useErc20Balance";
+export * from "./contracts/erc721/useErc721Name";
+export * from "./contracts/erc721/useErc721OwnerOf";
+export * from "./contracts/erc721/useErc721TokenUris";
+export * from "./contracts/erc1155/useErc1155Name";
+export * from "./contracts/erc1155/useErc1155Uris";
+export * from "./contracts/useGetTokenUriImages";
+export { useCoreSdk } from "./core-sdk/useCoreSdk";
+export * from "./form/useForm";
+export * from "./ipfs/useIpfsStorage";
+export * from "./location/buildUseSearchParams";
+export * from "./magic";
 export * from "./offer/useIsPhygital";
+export * from "./products/useProductByUuid";
+export * from "./storage/useLocalStorage";
+export * from "./uniswap/useIsWindowVisible";
+export * from "./useBreakpoints";
+export { useCtaClickHandler } from "./useCtaClickHandler";
+export * from "./useEffectDebugger";
+export * from "./useExchanges";
+export * from "./useHandleText";
+export { useIpfsMetadataStorage } from "./useIpfsMetadataStorage";
+export * from "./useMetaTx";
+export * from "./useMetaTx";
+export * from "./usePrevious";
+export * from "./useSignerAddress";
