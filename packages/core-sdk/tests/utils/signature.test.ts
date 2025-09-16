fa8e99ff67376457b5c31bd5e13e4c80503e4b93
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2025-09-16 10:56:27 +0100

    improve test coverage for utils/signature.ts

--- packages/core-sdk/tests/utils/signature.test.ts
+++ packages/core-sdk/tests/utils/signature.test.ts
@@ -33,6 +33,28 @@ describe("signature utils", () => {
     expect(sig).toBe(signature);
   });
 
+  test("getSignatureParameters() errors - signature is not a valid hex string", () => {
+    const signature =
+      "0xzz93bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a1b";
+    expect(() => getSignatureParameters(signature)).toThrow(
+      "not a valid hex string"
+    );
+  });
+
+  test("getSignatureParameters() errors - v is not a number", () => {
+    const signature =
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a";
+    const { r, s, v, signature: sig } = getSignatureParameters(signature);
+    expect(r).toBe(
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392"
+    );
+    expect(s).toBe(
+      "0x024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a"
+    );
+    expect(isNaN(v)).toBe(true);
+    expect(sig).toBe(signature);
+  });
+
   test("rebuildSignature()", () => {
     const r =
       "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392";
@@ -43,5 +65,27 @@ describe("signature utils", () => {
       "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a1b";
     const sig = rebuildSignature({ r, s, v });
     expect(sig).toBe(signature);
+  });
+
+  test("rebuildSignature() errors - r is not a valid hex string", () => {
+    const r =
+      "0xz093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392";
+    const s =
+      "0x024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a";
+    const v = 27;
+    expect(() => rebuildSignature({ r, s, v })).toThrow(
+      "r and s must be valid hex strings"
+    );
+  });
+
+  test("rebuildSignature() errors - s is not a valid hex string", () => {
+    const r =
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392";
+    const s =
+      "0xz24b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a";
+    const v = 27;
+    expect(() => rebuildSignature({ r, s, v })).toThrow(
+      "r and s must be valid hex strings"
+    );
   });
 });
