1bb36f369d2c2a145aae7ccb51866beed8b77d93
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2025-09-16 10:11:56 +0100

    improve test coverage for utils signature.ts

---
+++ packages/core-sdk/tests/utils/signature.test.ts
@@ -0,0 +1,47 @@
+import {
+  getSignatureParameters,
+  rebuildSignature
+} from "../../src/utils/signature";
+
+describe("signature utils", () => {
+  // minimal test to check that the function runs without error
+  test("getSignatureParameters()", () => {
+    const signature =
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a1b";
+    const { r, s, v, signature: sig } = getSignatureParameters(signature);
+    expect(r).toBe(
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392"
+    );
+    expect(s).toBe(
+      "0x024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a"
+    );
+    expect(v).toBe(27);
+    expect(sig).toBe(signature);
+  });
+
+  test("getSignatureParameters() - support Ledger signature", () => {
+    const signature =
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a01";
+    const { r, s, v, signature: sig } = getSignatureParameters(signature);
+    expect(r).toBe(
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392"
+    );
+    expect(s).toBe(
+      "0x024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a"
+    );
+    expect(v).toBe(28);
+    expect(sig).toBe(signature);
+  });
+
+  test("rebuildSignature()", () => {
+    const r =
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392";
+    const s =
+      "0x024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a";
+    const v = 27;
+    const signature =
+      "0xd093bf19f8e5d7526953f63b7721628b95820e94cf42298f97cd4502b61ff392024b4030ee7db3f74690e721289b287583d72ccd8ad297e69c822eb4f1f87c2a1b";
+    const sig = rebuildSignature({ r, s, v });
+    expect(sig).toBe(signature);
+  });
+});
