d715afac98c761a285c8ff252d273c220907aa64
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2025-09-16 15:43:09 +0100

    improve test coverage

--- packages/core-sdk/tests/meta-tx/handler.test.ts
+++ packages/core-sdk/tests/meta-tx/handler.test.ts
@@ -1,5 +1,14 @@
 import { MockWeb3LibAdapter } from "@bosonprotocol/common/tests/mocks";
-import { signMetaTxResolveDispute } from "../../src/meta-tx/handler";
+import {
+  getResubmitted,
+  signMetaTxPreMint,
+  signMetaTxResolveDispute,
+  signMetaTxSetApprovalForAll
+} from "../../src/meta-tx/handler";
+import * as mockInterface from "../../src/forwarder/mock-interface";
+import nock from "nock";
+
+jest.setTimeout(60_000);
 
 describe("meta-tx handler", () => {
   describe("#signMetaTxResolveDispute()", () => {
@@ -55,5 +64,120 @@ describe("meta-tx handler", () => {
       expect(signedMetaTx.s).toEqual(expectedSignedMetaTx.s);
       expect(signedMetaTx.v).toEqual(expectedSignedMetaTx.v);
     });
+  });
+  describe("#signMetaTxPreMint()", () => {
+    const biconomyUrl = "https://api.biconomy.io";
+    const expectedSignedMetaTx = {
+      signature:
+        "0x020d671b80fbd20466d8cb65cef79a24e3bca3fdf82e9dd89d78e7a4c4c045bd72944c20bb1d839e76ee6bb69fed61f64376c37799598b40b8c49148f3cdd88a1b",
+      r: "0x020d671b80fbd20466d8cb65cef79a24e3bca3fdf82e9dd89d78e7a4c4c045bd",
+      s: "0x72944c20bb1d839e76ee6bb69fed61f64376c37799598b40b8c49148f3cdd88a",
+      v: 27
+    };
+
+    test("call signMetaTxPreMint", async () => {
+      nock(biconomyUrl)
+        .get("/api/v2/meta-tx/systemInfo?networkId=1")
+        .reply(200, {
+          code: 200,
+          message: "OK",
+          forwarderDomainDetails: {
+            key1: {
+              verifyingContract: "0x0000000000000000000000000000000000000000",
+              name: "Biconomy Forwarder",
+              version: "1",
+              salt: "0x0000000000000000000000000000000000000000000000000000000000000001"
+            }
+          }
+        });
+
+      const signedMetaTx = await signMetaTxPreMint({
+        chainId: 1, // do not use local chain to force use of Biconomy methods
+        bosonVoucherAddress: "0x0000000000000000000000000000000000000000",
+        web3Lib: new MockWeb3LibAdapter({
+          getSignerAddress: "0x0000000000000000000000000000000000000001",
+          send: expectedSignedMetaTx.signature,
+          getChainId: 1,
+          call: "0x0000000000000000000000000000000000000000000000000000000000000042" // mock isTrustedForwarder call
+        }),
+        offerId: 1,
+        amount: 1,
+        relayerUrl: biconomyUrl,
+        forwarderAbi: mockInterface.abi
+      });
+      expect(signedMetaTx.r).toEqual(expectedSignedMetaTx.r);
+      expect(signedMetaTx.s).toEqual(expectedSignedMetaTx.s);
+      expect(signedMetaTx.v).toEqual(expectedSignedMetaTx.v);
+    });
+  });
+  describe("#signMetaTxSetApprovalForAll()", () => {
+    const biconomyUrl = "https://api.biconomy.io";
+    const expectedSignedMetaTx = {
+      signature:
+        "0x020d671b80fbd20466d8cb65cef79a24e3bca3fdf82e9dd89d78e7a4c4c045bd72944c20bb1d839e76ee6bb69fed61f64376c37799598b40b8c49148f3cdd88a1b",
+      r: "0x020d671b80fbd20466d8cb65cef79a24e3bca3fdf82e9dd89d78e7a4c4c045bd",
+      s: "0x72944c20bb1d839e76ee6bb69fed61f64376c37799598b40b8c49148f3cdd88a",
+      v: 27
+    };
+
+    test("call signMetaTxSetApprovalForAll()", async () => {
+      nock(biconomyUrl)
+        .get("/api/v2/meta-tx/systemInfo?networkId=1")
+        .reply(200, {
+          code: 200,
+          message: "OK",
+          forwarderDomainDetails: {
+            key1: {
+              verifyingContract: "0x0000000000000000000000000000000000000000",
+              name: "Biconomy Forwarder",
+              version: "1",
+              salt: "0x0000000000000000000000000000000000000000000000000000000000000001"
+            }
+          }
+        });
+
+      const signedMetaTx = await signMetaTxSetApprovalForAll({
+        chainId: 1, // do not use local chain to force use of Biconomy methods
+        bosonVoucherAddress: "0x0000000000000000000000000000000000000000",
+        web3Lib: new MockWeb3LibAdapter({
+          getSignerAddress: "0x0000000000000000000000000000000000000001",
+          send: expectedSignedMetaTx.signature,
+          getChainId: 1,
+          call: "0x0000000000000000000000000000000000000000000000000000000000000042" // mock isTrustedForwarder call
+        }),
+        operator: "0x0000000000000000000000000000000000000001",
+        approved: true,
+        relayerUrl: biconomyUrl,
+        forwarderAbi: mockInterface.abi
+      });
+      expect(signedMetaTx.r).toEqual(expectedSignedMetaTx.r);
+      expect(signedMetaTx.s).toEqual(expectedSignedMetaTx.s);
+      expect(signedMetaTx.v).toEqual(expectedSignedMetaTx.v);
+    });
+  });
+
+  test("getResubmitted", async () => {
+    const biconomyUrl = "https://api.biconomy.io";
+    nock(biconomyUrl)
+      .get("/api/v1/meta-tx/resubmitted?networkId=1&transactionHash=0x1234")
+      .reply(200, {
+        code: 200,
+        message: "OK",
+        data: {
+          key1: "value1"
+        }
+      });
+    const data = await getResubmitted({
+      chainId: 1,
+      metaTx: {
+        config: {
+          relayerUrl: biconomyUrl,
+          apiId: "1234",
+          apiKey: "abcd"
+        },
+        originalHash: "0x1234"
+      }
+    });
+    expect(data).toEqual({ key1: "value1" });
   });
 });
