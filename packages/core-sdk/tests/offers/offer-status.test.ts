f134d8ddd02694f5910f2e71e81ae5da7638a635
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2025-09-16 15:57:49 +0100

    improve test coverage

---
+++ packages/core-sdk/tests/offers/offer-status.test.ts
@@ -0,0 +1,33 @@
+import { getOfferStatus, OfferState } from "../../src/offers";
+
+describe("offer-status", () => {
+  let validOffer;
+  let now;
+  beforeEach(() => {
+    now = Math.floor(Date.now()/1000);
+    validOffer = {
+      voidedAt: null,
+      validFromDate: (now - 1).toString(),
+      validUntilDate: (now + 1).toString()
+    };
+  });
+  test("offer is valid", () => {
+    const offerStatus = getOfferStatus(validOffer);
+    expect(offerStatus).toBe(OfferState.VALID);
+  });
+  test("offer is not yet valid", () => {
+    validOffer.validFromDate = (now + 1).toString();
+    const offerStatus = getOfferStatus(validOffer);
+    expect(offerStatus).toBe(OfferState.NOT_YET_VALID);
+  });
+  test("offer is expired", () => {
+    validOffer.validUntilDate = (now - 1).toString();
+    const offerStatus = getOfferStatus(validOffer);
+    expect(offerStatus).toBe(OfferState.EXPIRED);
+  });
+  test("offer is voided", () => {
+    validOffer.voidedAt = (now - 1).toString();
+    const offerStatus = getOfferStatus(validOffer);
+    expect(offerStatus).toBe(OfferState.VOIDED);
+  });
+});
